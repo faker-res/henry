b28cc2dab18716106822ff18ed3700a0f789e248
ricco <ricco@snsoft.my>
2017-04-07 10:15:39 +0800

    Remove Copyrights Header (Phase 4)

--- Server/modules/dbproperties.js
+++ Server/modules/dbproperties.js
@@ -1,8 +1,3 @@
-/******************************************************************
- *  Fantasy Player Management Tool
- *  Copyright (C) 2015-2016 Sinonet Technology Singapore Pte Ltd.
- *  All rights reserved.
- ******************************************************************/
 'use strict';
 
 var dbPropertiesFunc = function () {
