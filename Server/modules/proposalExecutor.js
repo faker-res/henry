77416b879500b2be19dd2dbb41cdb67cd32eaa10
ricco <ricco@snsoft.my>
2017-04-07 10:21:05 +0800

    Remove Copyrights Header (Phase 5)

--- Server/modules/proposalExecutor.js
+++ Server/modules/proposalExecutor.js
@@ -1,8 +1,3 @@
-/******************************************************************
- *  Fantasy Player Management Tool
- *  Copyright (C) 2015-2016 Sinonet Technology Singapore Pte Ltd.
- *  All rights reserved.
- ******************************************************************/
 var proposalExecutorFunc = function () {
 };
 module.exports = new proposalExecutorFunc();
