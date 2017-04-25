84895d9ffd324caa23184a184167426222e1c001
vincentsnsoft <vincent@snsoft.my>
2017-04-25 18:07:06 +0800

    update sms api

--- Server/db_modules/dbPlayerMail.js
+++ Server/db_modules/dbPlayerMail.js
@@ -112,10 +112,10 @@ const dbPlayerMail = {
         );
     },
 
-    sendVerificationCodeToNumber: function (telNum, code) {
+    sendVerificationCodeToNumber: function (telNum, code, platformId) {
         let lastMin = moment().subtract(1, 'minutes');
         let a = smsAPI.channel_getChannelList({}).then(data => { return data });
-        let b = dbconfig.collection_platform.find().limit(1).then(data => { return data ? data[0] : null; });
+        let b = dbconfig.collection_platform.findOne({platformId: platformId}).lean();
         let c = dbconfig.collection_smsVerificationLog.findOne({tel: telNum, createTime: { $gt: lastMin}});
 
         return Q.all([a, b, c]).then(data => {
