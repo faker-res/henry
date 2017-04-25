3381ba0d4dbe9c4fd65914d4a5dfa2c3a60c4adb
ricco <ricco@snsoft.my>
2017-04-25 22:29:56 +0800

    #437 + Add platform setting to enable SMS verification + add player update payment info sms verification

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -992,14 +992,82 @@ let dbPlayerInfo = {
     /**
      * Update player payment info
      * @param {String}  query - The query string
-     * @param {string} updateData - The update data string
+     * @param {Object} updateData - The update data string
      */
     updatePlayerPayment: function (query, updateData) {
-        return dbUtility.findOneAndUpdateForShard(dbconfig.collection_players, query, updateData, constShardKeys.collection_players);
+        let playerObj = null;
+        // Get platform
+        return dbconfig.collection_players.findOne(query).lean().then(
+            playerData => {
+                if (playerData) {
+                    playerObj = playerData;
+
+                    return dbconfig.collection_platform.findOne({
+                        _id: playerData.platform
+                    })
+                }
+                else {
+                    return Q.reject({
+                        name: "DataError",
+                        code: constServerCode.DOCUMENT_NOT_FOUND,
+                        message: "Unable to find player"
+                    })
+                }
+            }
+        ).then(
+            platformData => {
+                if (platformData) {
+                    // Check if platform sms verification is required
+                    if (!platformData.requireSMSVerification) {
+                        // SMS verification not required
+                        return Q.resolve(true);
+                    } else {
+                        // Check verification SMS match
+                        return dbconfig.collection_smsVerificationLog.findOne({
+                            platformObjId: playerObj.platform,
+                            tel: playerObj.phoneNumber
+                        }).sort({createTime: -1}).then(
+                            verificationSMS => {
+                                // Check verification SMS code
+                                if (verificationSMS && verificationSMS.code && verificationSMS.code === updateData.modifyPaymentInfoSMSCode) {
+                                    verificationSMS = verificationSMS || {};
+                                    return dbconfig.collection_smsVerificationLog.remove(
+                                        {_id: verificationSMS._id}
+                                    ).then(
+                                        () => {
+                                            return Q.resolve(true);
+                                        }
+                                    )
+                                }
+                                else {
+                                    return Q.reject({
+                                        status: constServerCode.VALIDATION_CODE_INVALID,
+                                        name: "ValidationError",
+                                        message: "Invalid SMS Validation Code"
+                                    });
+                                }
+                            }
+                        )
+                    }
+                }
+                else {
+                    return Q.reject({
+                        name: "DataError",
+                        code: constServerCode.DOCUMENT_NOT_FOUND,
+                        message: "Unable to find platform"
+                    })
+                }
+            }
+        ).then(
+            isVerified => {
+                if (isVerified) {
+                    return dbUtility.findOneAndUpdateForShard(dbconfig.collection_players, query, updateData, constShardKeys.collection_players);
+                }
+            }
+        )
     },
 
     updatePlayerPaymentInfoCreateProposal: function (updateData) {
-
         var proposalData = updateData;
         if (updateData) {
             delete updateData.password;
