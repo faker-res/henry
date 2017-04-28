4f195524bb2c0918f77c7ded8723d29d4b2e7ae6
WenJie_Acer <singwj@snsoft.my>
2017-04-28 18:36:18 +0800

    - correction for partner status

--- Server/socketActionModule/socketActionPartner.js
+++ Server/socketActionModule/socketActionPartner.js
@@ -154,7 +154,8 @@ function socketActionPartner(socketIO, socket) {
             var actionName = arguments.callee.name;
             var isValidData = Boolean(data && data.platformId && data.query);
             var query = utility.buildPartnerQueryString(data.query);
-            socketUtil.emitter(self.socket, dbPartner.getPartnersByAdvancedQuery, [data.platformId, data.query], actionName, isValidData);
+
+            socketUtil.emitter(self.socket, dbPartner.getPartnersByAdvancedQuery, [data.platformId, query], actionName, isValidData);
         },
 
         /**
