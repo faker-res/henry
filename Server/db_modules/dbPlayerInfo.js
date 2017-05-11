a142ae63283cec83365d55a367de5b825740ce0c
ricco <ricco@snsoft.my>
2017-05-11 17:51:58 +0800

    #656 修复游戏提供商禁用功能 - Test fix

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -6226,14 +6226,13 @@ let dbPlayerInfo = {
 
                     let providerEnabled = true;
 
-                    if (playerData.platform.gameProviderInfo[String(gameData.provider._id)]
-                        && !(playerData.platform.gameProviderInfo[String(gameData.provider._id)].isEnabled)) {
+                    if (playerData.platform.gameProviderInfo[gameData.provider._id]
+                        && playerData.platform.gameProviderInfo[gameData.provider._id].isEnabled == false) {
                         providerEnabled = false;
                     }
 
                     // Added checking for platform level disable game provider
-                    if (gameData.provider.status != constProviderStatus.NORMAL
-                        || !providerEnabled) {
+                    if (gameData.provider.status != constProviderStatus.NORMAL || !providerEnabled) {
                         return Q.reject({
                             status: constServerCode.CP_NOT_AVAILABLE,
                             name: "DataError",
@@ -6241,6 +6240,7 @@ let dbPlayerInfo = {
                             providerStatus: gameData.provider.status
                         });
                     }
+
                     return dbconfig.collection_platformGameStatus.findOne({
                         platform: playerData.platform._id,
                         game: gameData._id
