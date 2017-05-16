50700a9ef4886b7f604c2fa55ea48913f3221de4
ricco <ricco@snsoft.my>
2017-05-16 15:09:33 +0800

    #633 添加通过短信验证码修改玩家手机号码的api - added player partner functionality

--- Server/services/client/ClientServices.js
+++ Server/services/client/ClientServices.js
@@ -42,6 +42,7 @@
                 "getPlayerPartner",
                 "update",
                 "updatePhoneNumberWithSMS",
+                "updatePlayerPartnerPhoneNumberWithSMS",
                 "captcha",
                 "login",
                 "loginPlayerPartner",
