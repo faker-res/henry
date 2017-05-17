37c6d2acaba712cfb400c314e0e469d603c6a137
ricco <ricco@snsoft.my>
2017-05-17 16:25:52 +0800

    Add rewardRate factor when calculate partner commission

--- Server/db_modules/dbPartner.js
+++ Server/db_modules/dbPartner.js
@@ -2729,8 +2729,8 @@ let dbPartner = {
                         serviceFee = (totalTopUpAmount + totalPlayerBonusAmount) * configData.serviceFeeRate;
                     }
 
-                    if (rewardInfo && rewardInfo[0]) {
-                        totalRewardAmount = rewardInfo[0].totalRewardAmount;
+                    if (rewardInfo && rewardInfo[0] && configData && configData.rewardRate) {
+                        totalRewardAmount = rewardInfo[0].totalRewardAmount * configData.rewardRate;
                     }
 
                     switch (configData.settlementMode) {
@@ -3571,8 +3571,8 @@ let dbPartner = {
                     // operationAmount = totalBonusAmount;
                     platformFeeAmount = Math.abs(totalBonusAmount);
                 }
-                if (rewardInfo && rewardInfo[0]) {
-                    totalRewardAmount = rewardInfo[0].totalRewardAmount;
+                if (rewardInfo && rewardInfo[0] && configData && configData.rewardRate) {
+                    totalRewardAmount = rewardInfo[0].totalRewardAmount * configData.rewardRate;
                 }
                 if (configData && configData.serviceFeeRate > 0) {
                     serviceFee = (totalTopUpAmount + totalPlayerBonusAmount) * configData.serviceFeeRate;
