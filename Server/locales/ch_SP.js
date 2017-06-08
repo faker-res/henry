9e234f486abc6f0ce00a375cba01419f83feceb7
vincentsnsoft <vincent@snsoft.my>
2017-06-08 16:06:46 +0800

    update error message

--- Server/locales/ch_SP.js
+++ Server/locales/ch_SP.js
@@ -152,7 +152,8 @@ var simplifiedChinese = {
     "Please login and try again": "请登陆账号后再试",
     "Invalid SMS Validation Code": "验证码已过期，请重新获取验证码",
     "Verification SMS already sent within last minute": "验证码已发送，请稍后再试",
-    "Invalid referral": "推荐人不存在"
+    "Invalid referral": "推荐人不存在",
+    "Player does not have enough top up or consumption amount": "玩家没有达到奖励要求"
     //todo::add missing messages here
 
     /*
