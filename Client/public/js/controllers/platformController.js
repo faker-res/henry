51c13a3567391025f17a53ec65a3c188d6431744
huat <huat@snsoft.my>
2017-06-20 11:17:19 +0800

    添加群发站内信功能 #1030

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -660,6 +660,7 @@ define(['js/app'], function (myApp) {
                 numReceived: 0,
                 numFailed: 0,
                 numRecipient: 0,
+                messageType: "sms",
                 sendBtnText: $translate("SEND")
             };
             $scope.getChannelList(function () {
@@ -874,19 +875,35 @@ define(['js/app'], function (myApp) {
                 }
                 updateMultiMessageButton();
                 $scope.safeApply();
-            })
-            vm.sendMultiMessage.tableObj.rows('.selected').data().each(function (data) {
-                $scope.AppSocket.emit('sendSMSToPlayer', {
-                    playerId: data.playerId,
-                    platformId: vm.selectedPlatform.data.platformId,
-                    channel: vm.sendMultiMessage.channel,
-                    message: vm.sendMultiMessage.messageContent
-                })
-            })
+            });
+            if (vm.sendMultiMessage.messageType === "sms") {
+                vm.sendMultiMessage.tableObj.rows('.selected').data().each(function (data) {
+                    $scope.AppSocket.emit('sendSMSToPlayer', {
+                        playerId: data.playerId,
+                        platformId: vm.selectedPlatform.data.platformId,
+                        channel: vm.sendMultiMessage.channel,
+                        message: vm.sendMultiMessage.messageContent
+                    });
+                });
+            } else if (vm.sendMultiMessage.messageType === "mail") {
+                vm.sendMultiMessage.tableObj.rows('.selected').data().each(function (data) {
+                    let sendData = {
+                        playerId: data._id,
+                        adminName: authService.adminName,
+                        platformId: vm.selectedPlatform.id,
+                        title: vm.sendMultiMessage.messageTitle,
+                        content: vm.sendMultiMessage.messageContent
+                    };
+                    // console.log('_sendData', sendData);
+                    $scope.AppSocket.emit('sendPlayerMailFromAdminToPlayer', sendData);
+                });
+            }
+
             vm.sendMultiMessage.sendInitiated = true;
+            vm.sendMultiMessage.messageTitle = "";
             vm.sendMultiMessage.messageContent = "";
             updateMultiMessageButton();
-        }
+        };
 
         vm.sendSingleMessages = function () {
             vm.sendMultiMessage.singleBtnText = $translate("Sending");
@@ -3298,7 +3315,7 @@ define(['js/app'], function (myApp) {
         vm.playerTableRowClicked = function (rowData) {
             var deferred = Q.defer();
             console.log('player', rowData);
-            var sendData = {_id: rowData._id}
+            var sendData = {_id: rowData._id};
             socketService.$socket($scope.AppSocket, 'getOnePlayerInfo', sendData, function (retData) {
                 var player = retData.data;
                 console.log('updated info');
