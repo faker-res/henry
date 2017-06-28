249cb0e4d83adcf7c24684f3a9b3346e190d073a
vincentsnsoft <vincent@snsoft.my>
2017-06-28 16:41:38 +0800

    update get alipay single limit api

--- Server/db_modules/dbPlayerPayment.js
+++ Server/db_modules/dbPlayerPayment.js
@@ -9,28 +9,28 @@ const dbPlayerPayment = {
      * @param {String} playerId - The data of the PlayerTrustLevel. Refer to PlayerTrustLevel schema.
      */
     getAlipaySingleLimit : (playerId) => {
-        var playerData = null;
+        let playerData = null;
         return dbconfig.collection_players.findOne({playerId: playerId}).populate(
             {path: "platform", model: dbconfig.collection_platform}
         ).populate(
             {path: "alipayGroup", model: dbconfig.collection_platformAlipayGroup}
         ).lean().then(
             data => {
-                if (data && data.platform) {
+                if (data && data.platform && data.alipayGroup) {
                     playerData = data;
                         return pmsAPI.alipay_getAlipayList({
                             platformId: data.platform.platformId,
                             queryId: serverInstance.getQueryId()
                         });
                 } else {
-                    return Q.reject({name: "DataError", message: "Cannot find player"})
+                    return Q.reject({name: "DataError", message: "Invalid player data"})
                 }
             }
         ).then(
             alipays => {
                 let bValid = false;
                 let singleLimit = 0;
-                if (alipays.data && alipays.data.length > 0) {
+                if (alipays && alipays.data && alipays.data.length > 0) {
                     alipays.data.forEach(
                         alipay => {
                             playerData.alipayGroup.alipays.forEach(
