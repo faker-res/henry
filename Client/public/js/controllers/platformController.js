aaf421a67a36dabdaca85a440a8b5c483f6c0d4b
huat <huat@snsoft.my>
2017-07-06 13:56:46 +0800

    平台，游戏组，增加gameid和desciprtion搜索选项 #1168

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -1505,6 +1505,8 @@ define(['js/app'], function (myApp) {
                 if (vm.filterGameType && (vm.filterGameType != 'all') && (vm.filterGameType != item.type)) return false;
                 if (vm.filterPlayGameType && (vm.filterPlayGameType != 'all') && (vm.filterPlayGameType != item.playGameType)) return false;
                 if (vm.filterGameName && item.name.indexOf(vm.filterGameName) == -1) return false;
+                if (vm.filterGameId && item.gameId.indexOf(vm.filterGameId) == -1) return false;
+                if (vm.filterGameDescription && item.description.indexOf(vm.filterGameDescription) == -1) return false;
                 if (filterProvider && vm.filterGameProvider && (vm.filterGameProvider != 'all') && (vm.filterGameProvider != item.provider)) return false;
                 if (item.status == 4) return false;
                 return true;
