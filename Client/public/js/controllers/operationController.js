ebaf1dd21339e3920ceebaa404a86f580f387a81
WenJie_Acer <singwj@snsoft.my>
2017-07-19 14:10:56 +0800

    change background color for proposal table when credit >= 50000

--- Client/public/js/controllers/operationController.js
+++ Client/public/js/controllers/operationController.js
@@ -1321,7 +1321,7 @@ define(['js/app'], function (myApp) {
                     break;
                 }
                 case (aData.creditAmount$ >= 50000 && aData.creditAmount$ < 500000): {
-                    $(nRow).css('background-color', 'rgba(195, 39, 43, 100)');
+                    $(nRow).css('background-color', 'rgba(231, 76, 60, 100)');
                     break;
                 }
                 case (aData.creditAmount$ >= 500000 && aData.creditAmount$ < 1000000): {
