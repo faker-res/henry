b8ecb66f66d9de1ad503ff68a496ec966243fb9c
huat <huat@snsoft.my>
2017-07-31 15:26:52 +0800

    Boilerplate for Monitor section

--- Client/public/js/app.js
+++ Client/public/js/app.js
@@ -57,9 +57,19 @@ define(['js/config', 'js/commonAPIs', 'js/services/routeResolver', 'js/services/
             //     templateUrl: 'category/dashboard/dashboard',
             //     //controller: 'dashboardCtrl'
             // }).
-            when('/dashboard', route.resolve('category/dashboard/main', 'dashboardCtrl', 'dashboardController', 'vm')).when('/mainPage', route.resolve('category/mainPage/main', 'mainPageCtrl', 'mainPageController', 'vm')).// when('/proposal', route.resolve('category/proposal/proposal-home', 'proposalCtrl', 'proposalController', 'vm')).
-            when('/platform', route.resolve('category/platform/platform-home', 'platformCtrl', 'platformController', 'vm')).when('/payment', route.resolve('category/payment/payment-home', 'paymentCtrl', 'paymentController', 'vm')).when('/provider', route.resolve('category/provider/provider-home', 'providerCtrl', 'providerController', 'vm')).when('/operation', route.resolve('category/operation/operation-home', 'operationCtrl', 'operationController', 'vm')).// when('/reward', route.resolve('category/reward/reward-home', 'rewardCtrl', 'rewardController', 'vm')).
-            when('/analysis', route.resolve('category/analysis/analysis-home', 'analysisCtrl', 'analysisController', 'vm')).when('/report', route.resolve('category/report/report-home', 'reportCtrl', 'reportController', 'vm')).when('/testPage', route.resolve('category/testPage/test-home', 'testRewardCtrl', 'testRewardController', 'vm')).otherwise({
+            when('/dashboard', route.resolve('category/dashboard/main', 'dashboardCtrl', 'dashboardController', 'vm')).
+            when('/mainPage', route.resolve('category/mainPage/main', 'mainPageCtrl', 'mainPageController', 'vm')).
+            // when('/proposal', route.resolve('category/proposal/proposal-home', 'proposalCtrl', 'proposalController', 'vm')).
+            when('/platform', route.resolve('category/platform/platform-home', 'platformCtrl', 'platformController', 'vm')).
+            when('/payment', route.resolve('category/payment/payment-home', 'paymentCtrl', 'paymentController', 'vm')).
+            when('/provider', route.resolve('category/provider/provider-home', 'providerCtrl', 'providerController', 'vm')).
+            when('/operation', route.resolve('category/operation/operation-home', 'operationCtrl', 'operationController', 'vm')).
+            // when('/reward', route.resolve('category/reward/reward-home', 'rewardCtrl', 'rewardController', 'vm')).
+            when('/analysis', route.resolve('category/analysis/analysis-home', 'analysisCtrl', 'analysisController', 'vm')).
+            when('/report', route.resolve('category/report/report-home', 'reportCtrl', 'reportController', 'vm')).
+            when('/monitor', route.resolve('category/monitor/monitor-home', 'monitorCtrl', 'monitorController', 'vm')).
+            when('/testPage', route.resolve('category/testPage/test-home', 'testRewardCtrl', 'testRewardController', 'vm')).
+            otherwise({
                 redirectTo: '/dashboard'
             });
             $locationProvider.html5Mode(true);
---
+++ Client/public/js/controllers/monitorController.js
@@ -0,0 +1,18 @@
+'use strict';
+
+define(['js/app'], function (myApp) {
+    let injectParams = ['$sce', '$scope', '$filter', '$compile', '$location', '$log', 'socketService', 'authService', 'utilService', 'CONFIG', "$cookies"];
+    let monitorController = function ($sce, $scope, $filter, $compile, $location, $log, socketService, authService, utilService, CONFIG, $cookies) {
+        let $translate = $filter('translate');
+        let vm = this;
+
+        // For debugging:
+        window.VM = vm;
+
+
+    };
+
+    monitorController.$inject = injectParams;
+
+    myApp.register.controller('monitorCtrl', monitorController);
+});
