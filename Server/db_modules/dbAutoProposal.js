721bbcfe60a3340c0c0b63310f101b359ae51883
ricco <ricco@snsoft.my>
2017-08-22 10:38:11 +0800

    Auto Audit - add devCheckMsg

--- Server/db_modules/dbAutoProposal.js
+++ Server/db_modules/dbAutoProposal.js
@@ -378,6 +378,7 @@ function checkProposalConsumption(proposal, platformObj) {
                     let totalConsumptionAmount = 0, totalSpendingAmount = 0;
                     let lastTopUpResult = {};
                     let currentProposal = null;
+                    let devCheckMsg = "";
 
                     // Make sure the check result is in correct order
                     checkResult.sort((a, b) => a.settleTime.getTime() - b.settleTime.getTime());
@@ -400,8 +401,6 @@ function checkProposalConsumption(proposal, platformObj) {
 
                         totalConsumptionAmount += checkResult[i].curConsumption ? checkResult[i].curConsumption : 0;
                         totalSpendingAmount += checkResult[i].requiredConsumption ? checkResult[i].requiredConsumption : 0;
-
-                        // checkMsg += "ProposalId:" + checkResult[i].proposalId + " requiredConsumption:" + checkResult[i].requiredConsumption + ", ";
 
                         if (checkResult[i].initBonusAmount) {
                             initBonusAmount += checkResult[i].initBonusAmount ? checkResult[i].initBonusAmount : 0;
@@ -427,6 +426,8 @@ function checkProposalConsumption(proposal, platformObj) {
                             initBonusAmount += lastTopUpResult.initBonusAmount ? lastTopUpResult.initBonusAmount : 0;
                             bonusAmount += lastTopUpResult.bonusAmount ? lastTopUpResult.bonusAmount : 0;
                         }
+
+                        devCheckMsg += currentProposal + ": " + "Bonus: " + bonusAmount + "/" + initBonusAmount + ", Consumption: " + validConsumptionAmount + "/" + spendingAmount + "; ";
 
                         // Check consumption for each cycle
                         if (initBonusAmount && initBonusAmount != 0 && initBonusAmount + bonusAmount <= lostThreshold) {
@@ -514,7 +515,7 @@ function checkProposalConsumption(proposal, platformObj) {
                     // Check consumption approved or not
                     if (isApprove || isTypeEApproval) {
                         if (!canApprove) {
-                            sendToAudit(proposal._id, proposal.createTime, checkMsg, checkMsgChinese, null, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese);
+                            sendToAudit(proposal._id, proposal.createTime, checkMsg, checkMsgChinese, null, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese, devCheckMsg);
                         } else {
                             let approveRemark = "Success: Consumption " + validConsumptionAmount + ", Required Bet " + spendingAmount;
                             let approveRemarkChinese = "成功：流水 " + validConsumptionAmount + "，所需流水 " + spendingAmount;
@@ -557,10 +558,10 @@ function checkProposalConsumption(proposal, platformObj) {
                             // Check if player is VIP - Passed
                             if (proposal.data.proposalPlayerLevelValue == 0) {
                                 //sendToReject(proposal._id, proposal.createTime, "Denied: Non-VIP: Exceed Auto Approval Repeat Limit", "失败：非VIP：超出回圈次数", checkMsg, abnormalMessage, abnormalMessageChinese);
-                                sendToAudit(proposal._id, proposal.createTime, checkMsg, checkMsgChinese, null, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese);
+                                sendToAudit(proposal._id, proposal.createTime, checkMsg, checkMsgChinese, null, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese, devCheckMsg);
                             } else {
                                 //sendToReject(proposal._id, proposal.createTime, "Denied: VIP: Exceed Auto Approval Repeat Limit", "失败：VIP：超出回圈次数", checkMsg, abnormalMessage, abnormalMessageChinese);
-                                sendToAudit(proposal._id, proposal.createTime, checkMsg, checkMsgChinese, null, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese);
+                                sendToAudit(proposal._id, proposal.createTime, checkMsg, checkMsgChinese, null, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese, devCheckMsg);
                             }
                         }
 
@@ -646,7 +647,7 @@ function sendToReject(proposalObjId, createTime, remark, remarkChinese, processR
     );
 }
 
-function sendToAudit(proposalObjId, createTime, remark, remarkChinese, processRemark, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese) {
+function sendToAudit(proposalObjId, createTime, remark, remarkChinese, processRemark, abnormalMessage, abnormalMessageChinese, repeatMsg, repeatMsgChinese, devCheckMsg) {
     processRemark = processRemark ? processRemark : "";
 
     dbconfig.collection_proposal.findOne({_id: proposalObjId}).populate({
@@ -668,7 +669,8 @@ function sendToAudit(proposalObjId, createTime, remark, remarkChinese, processRe
                         'data.detail': abnormalMessage ? abnormalMessage : "",
                         'data.detailChinese': abnormalMessageChinese ? abnormalMessageChinese : "",
                         'data.autoAuditRepeatMsg': repeatMsg,
-                        'data.autoAuditRepeatMsgChinese': repeatMsgChinese
+                        'data.autoAuditRepeatMsgChinese': repeatMsgChinese,
+                        'data.devCheckMsg': devCheckMsg
                     }).then();
                 }
                 else {
