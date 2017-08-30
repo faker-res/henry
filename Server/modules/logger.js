f48b614dae859b166129bd977c85a46593549da3
vincentsnsoft <vincent@snsoft.my>
2017-08-30 22:14:44 +0800

    disable logger

--- Server/modules/logger.js
+++ Server/modules/logger.js
@@ -1,44 +1,44 @@
-var winston = require('winston');
-var MongoDB = require('winston-mongodb').MongoDB;
-var env = require('./../config/env').config();
+// var winston = require('winston');
+// var MongoDB = require('winston-mongodb').MongoDB;
+// var env = require('./../config/env').config();
 
 // Logging levels
-var config = {
-    levels: {
-        error: 0,
-        debug: 1,
-        warn: 2,
-        data: 3,
-        info: 4,
-        verbose: 5,
-        silly: 6
-    },
-    colors: {
-        error: 'red',
-        debug: 'blue',
-        warn: 'yellow',
-        data: 'grey',
-        info: 'green',
-        verbose: 'cyan',
-        silly: 'magenta'
-    }
-};
-
-var emptyLogger = new (winston.Logger)({});
-
-//logger for display log at console
-var consoleLogger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            colorize: true,
-            prettyPrint: true,
-            silent: false,
-            timestamp: false
-        })
-    ],
-    levels: config.levels,
-    colors: config.colors
-});
+// var config = {
+//     levels: {
+//         error: 0,
+//         debug: 1,
+//         warn: 2,
+//         data: 3,
+//         info: 4,
+//         verbose: 5,
+//         silly: 6
+//     },
+//     colors: {
+//         error: 'red',
+//         debug: 'blue',
+//         warn: 'yellow',
+//         data: 'grey',
+//         info: 'green',
+//         verbose: 'cyan',
+//         silly: 'magenta'
+//     }
+// };
+//
+// var emptyLogger = new (winston.Logger)({});
+//
+// //logger for display log at console
+// var consoleLogger = new (winston.Logger)({
+//     transports: [
+//         new (winston.transports.Console)({
+//             colorize: true,
+//             prettyPrint: true,
+//             silent: false,
+//             timestamp: false
+//         })
+//     ],
+//     levels: config.levels,
+//     colors: config.colors
+// });
 
 //logger for store log into database
 // var dbLogger = new (winston.Logger)({
@@ -52,7 +52,7 @@ var consoleLogger = new (winston.Logger)({
 
 var loggers = {
     //console logger
-    conLog: env.mode !== 'production' ? consoleLogger : emptyLogger,
+    // conLog: env.mode !== 'production' ? consoleLogger : emptyLogger,
     //database logger
     //dbLog: dbLogger
 };
