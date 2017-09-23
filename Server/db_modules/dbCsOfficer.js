9874f2e13bd0f0db676f88554843db5f71fd234f
zheming <zheming@snsoft.my>
2017-09-23 17:04:23 +0800

    create officer implementation

--- Server/db_modules/dbCsOfficer.js
+++ Server/db_modules/dbCsOfficer.js
@@ -1,8 +1,23 @@
 const dbconfig = require('./../modules/dbproperties');
+var Q = require("q");
 
 let dbCsOfficer = {
 
+   createOfficer: function(platformId, name){
+       return dbconfig.collection_csOfficer.findOne({platform: platformId, name: name}).lean().then(
+           function (data) {
+               if (data) {
+                   return Promise.reject({
+                       name: "DataError",
+                       message: "Name already existed"
+                   });
+               }
 
+               let newOfficer = dbconfig.collection_csOfficer({platform: platformId, name: name});
+               return newOfficer.save().then().catch(err => errorSavingLog(err, adminActionRecordData));
+           }
+       );
+   }
 
     ////
 
