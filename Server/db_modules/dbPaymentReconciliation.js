00886fbbb973c752c46b6632a1b7cdf17ff35eba
huat <huat@snsoft.my>
2017-09-25 12:15:32 +0800

    在线支付丢失报表 增加选项

--- Server/db_modules/dbPaymentReconciliation.js
+++ Server/db_modules/dbPaymentReconciliation.js
@@ -5,43 +5,89 @@ const constProposalType = require("../const/constProposalType");
 const constProposalStatus = require("../const/constProposalStatus");
 
 const dbPaymentReconciliation = {
-    testAPI: function (platform, platformId, startTime, endTime) {
-        return pmsAPI.reconciliation_getOnlineCashinList({
-            platformId: platformId,
-            starttime: startTime,
-            endtime: endTime
-        });
-    },
 
-    getOnlinePaymentProposalMismatchReport: function (platform, platformId, startTime, endTime) {
+    getOnlinePaymentProposalMismatchReport: function (platform, platformId, option, startTime, endTime) {
         // since PMS does not allow having more than 24hr between time frame,
         // FPMS will do the time slicing internally
         let timeFrames = sliceTimeFrameToDaily(startTime, endTime);
 
-        return dbconfig.collection_proposalType.findOne({platformId: platform, name: constProposalType.PLAYER_TOP_UP}).lean().then(
+        let name = {
+            $in: [
+                constProposalType.PLAYER_TOP_UP,
+                constProposalType.PLAYER_ALIPAY_TOP_UP,
+                constProposalType.PLAYER_WECHAT_TOP_UP,
+                constProposalType.PLAYER_MANUAL_TOP_UP,
+                constProposalType.PLAYER_QUICKPAY_TOP_UP
+            ]
+        };
+
+        if (option === 'online') {
+            name = constProposalType.PLAYER_TOP_UP;
+        }
+        else if (option === 'manual') {
+            name = {
+                $in: [
+                    constProposalType.PLAYER_ALIPAY_TOP_UP,
+                    constProposalType.PLAYER_WECHAT_TOP_UP,
+                    constProposalType.PLAYER_MANUAL_TOP_UP,
+                    constProposalType.PLAYER_QUICKPAY_TOP_UP
+                ]
+            };
+        }
+
+        return dbconfig.collection_proposalType.find({platformId: platform, name: name}).lean().then(
             proposalTypeData => {
-                let proposalTypeId = proposalTypeData._id;
+                let proposalType = proposalTypeData;
                 let allProm = [];
 
+                let proposalTypeIds = [];
+
+                for (let i = 0, len = proposalType.length; i < len; i++) {
+                    proposalTypeIds.push(proposalType[i]._id);
+                }
+
                 for (let t = 0, tLength = timeFrames.length; t < tLength; t++) {
+                    let promises = [];
                     let start = timeFrames[t].startTime;
                     let end = timeFrames[t].endTime;
 
-                    let pmsProm = pmsAPI.reconciliation_getOnlineCashinList({
+                    let pmsProm = pmsAPI.reconciliation_getCashinList({
                         platformId: platformId,
                         starttime: startTime,
                         endtime: endTime
                     });
 
+                    if (option === 'online') {
+                        pmsProm = pmsAPI.reconciliation_getOnlineCashinList({
+                            platformId: platformId,
+                            starttime: startTime,
+                            endtime: endTime
+                        });
+                    }
+
+                    promises.push(pmsProm);
+
                     let proposalQuery = {
-                        type: proposalTypeId,
+                        type: {$in: proposalTypeIds},
                         status: {$in: [constProposalStatus.SUCCESS, constProposalStatus.APPROVED]},
                         createTime: {$gte: start, $lte: end}
                     };
 
                     let proposalProm = dbconfig.collection_proposal.find(proposalQuery, {proposalId: 1, "data.amount": 1, createTime: 1}).lean();
 
-                    allProm.push(Promise.all([pmsProm, proposalProm]));
+                    promises.push(proposalProm);
+
+                    if (option === 'manual') {
+                        let pmsOnlineCashinProm = pmsAPI.reconciliation_getCashinList({
+                            platformId: platformId,
+                            starttime: startTime,
+                            endtime: endTime
+                        });
+
+                        promises.push(pmsOnlineCashinProm);
+                    }
+
+                    allProm.push(Promise.all(promises));
                 }
 
                 return Promise.all(allProm);
@@ -51,7 +97,7 @@ const dbPaymentReconciliation = {
                 let mismatches = [];
 
                 for (let i = 0, len = proposalGroup.length; i < len; i++) {
-                    let mismatchWithinGroup = getMismatchFromProposalGroup(proposalGroup[i]);
+                    let mismatchWithinGroup = getMismatchFromProposalGroup(proposalGroup[i], Boolean(option === 'manual'));
                     mismatches = mismatches.concat(mismatchWithinGroup);
                 }
 
@@ -91,9 +137,25 @@ function sliceTimeFrameToDaily(startTime, endTime) {
     return timeFrames;
 }
 
-function getMismatchFromProposalGroup(proposals) {
+function getMismatchFromProposalGroup(proposals, isManual) {
     let pmsProposals = proposals[0].onlineCashinList;
     let localProposals = proposals[1];
+    let pmsOnlineProposals = isManual ? proposals[2] : [];
+
+    if (isManual) {
+        for (let i = 0, iLength = pmsOnlineProposals.length; i < iLength; i++) {
+            for (let j = 0, jLength = pmsProposals.length; j < jLength; j++) {
+                let pmsOnlineProposal = pmsOnlineProposals[i];
+                let pmsProposal = pmsProposals[j];
+
+                if (pmsProposal.proposalId && pmsOnlineProposal.proposalId && pmsProposal.proposalId.toString() === pmsOnlineProposal.proposalId.toString()) {
+                    pmsProposal.matched = true;
+                    pmsOnlineProposal.matched = true;
+                    break;
+                }
+            }
+        }
+    }
 
     for (let i = 0, iLength = pmsProposals.length; i < iLength; i++) {
         for (let j = 0, jLength = localProposals.length; j < jLength; j++) {
