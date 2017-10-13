482f6015a14b9adda54dd5e265937373d65190c9
linhao <mars@snsoft.my>
2017-10-13 14:14:27 +0800

    #2361 添加投注记录收录报告(added null checking if there is no setting)

--- Server/db_modules/dbPlayerConsumptionRecord.js
+++ Server/db_modules/dbPlayerConsumptionRecord.js
@@ -1624,19 +1624,23 @@ var dbPlayerConsumptionRecord = {
                 var latestCreateTime = new Date(lastestConsumptionRecord.createTime);
                 var difference = currentDate - latestCreateTime ;
                 var resultInMinutes = Math.round(difference / 60000);
-                var recordStatus = {createTime: latestCreateTime};
+                var recordStatus = {createTime: latestCreateTime, delayStatusColor:"rgb(255,255,255)" };
+
+
 
                 let consumptionTimeConfig = platform.consumptionTimeConfig;
 
-                consumptionTimeConfig = consumptionTimeConfig.sort(function(configA, configB){
-                    return configA.duration - configB.duration;
-                });
+                if(platform){
+                    consumptionTimeConfig = consumptionTimeConfig.sort(function(configA, configB){
+                        return configA.duration - configB.duration;
+                    });
 
-
-                for(let i =0; i < consumptionTimeConfig.length; i ++) {
-                    recordStatus.delayStatusColor = consumptionTimeConfig[i].color;
-                    if (resultInMinutes <= consumptionTimeConfig[i].duration) break;
+                    for(let i =0; i < consumptionTimeConfig.length; i ++) {
+                        recordStatus.delayStatusColor = consumptionTimeConfig[i].color;
+                        if (resultInMinutes <= consumptionTimeConfig[i].duration) break;
+                    }
                 }
+
 
                 return recordStatus;
             }
