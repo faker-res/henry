b1f7ccb5e4ac4496b8bf9971b8c80be1da4b57fe
vincentsnsoft <vincent@snsoft.my>
2017-10-14 09:56:45 +0800

    update sms api

--- Server/externalAPI/smsAPI.js
+++ Server/externalAPI/smsAPI.js
@@ -7,69 +7,69 @@ const request = require('request');
 const dbLogger = require("../modules/dbLogger")
 const clientAPIInstance = require("../modules/clientApiInstances");
 
-// function callSMSAPI(service, functionName, data){
-//     if( !data ){
-//         return Q.reject(new Error("Invalid data!"));
-//     }
-//     var wsClient = serverInstance.getSMSAPIClient();
-//     if( !wsClient || !wsClient.isOpen() ){
-//         return Q.reject({ status: 400, errMessage: "Invalid WebSocket client connection!  (No SMSAPI stored for this instance.)"});
-//     }
-//     return wsClient.callAPIOnce(service, functionName, data);
-// };
-
-function callSMSAPI(service, functionName, data) {
-    if (!data) {
+function callSMSAPI(service, functionName, data){
+    if( !data ){
         return Q.reject(new Error("Invalid data!"));
     }
-    // var wsClient = serverInstance.getPaymentAPIClient();
-    // if (!wsClient || !wsClient.isOpen()) {
-    //     return Q.reject({
-    //         status: 400,
-    //         errMessage: "Invalid WebSocket client connection!  (No SMSAPI stored for this instance.)"
-    //     });
-    // }
-    let bOpen = false;
-    var deferred = Q.defer();
-    //if can't connect in 30 seconds, treat as timeout
-    setTimeout(function(){
-        if( !bOpen ){
-            return deferred.reject({
-                status: 400,
-                message: "SMS is not available"
-            });
-        }
-    }, 5*1000);
-    clientAPIInstance.createAPIConnectionInMode("SMSAPI").then(
-        con => {
-            bOpen = true;
-            return con.callAPIOnce(service, functionName, data).then(
-                data => {
-                    if (con && typeof con.disconnect == "function") {
-                        con.disconnect();
-                    }
-                    return data;
-                },
-                error => {
-                    if (con && typeof con.disconnect == "function") {
-                        con.disconnect();
-                    }
-                    if (error.status) {
-                        return Q.reject(error);
-                    }
-                    else {
-                        return Q.reject({
-                            status: 400,
-                            message: "SMS is not available",
-                            error: error
-                        });
-                    }
-                }
-            );
-        }
-    ).then(deferred.resolve, deferred.reject);
-    return deferred.promise;
+    var wsClient = serverInstance.getSMSAPIClient();
+    if( !wsClient || !wsClient.isOpen() ){
+        return Q.reject({ status: 400, message: "SMS is not available"});
+    }
+    return wsClient.callAPIOnce(service, functionName, data);
 };
+
+// function callSMSAPI(service, functionName, data) {
+//     if (!data) {
+//         return Q.reject(new Error("Invalid data!"));
+//     }
+//     // var wsClient = serverInstance.getPaymentAPIClient();
+//     // if (!wsClient || !wsClient.isOpen()) {
+//     //     return Q.reject({
+//     //         status: 400,
+//     //         errMessage: "Invalid WebSocket client connection!  (No SMSAPI stored for this instance.)"
+//     //     });
+//     // }
+//     let bOpen = false;
+//     var deferred = Q.defer();
+//     //if can't connect in 30 seconds, treat as timeout
+//     setTimeout(function(){
+//         if( !bOpen ){
+//             return deferred.reject({
+//                 status: 400,
+//                 message: "SMS is not available"
+//             });
+//         }
+//     }, 5*1000);
+//     clientAPIInstance.createAPIConnectionInMode("SMSAPI").then(
+//         con => {
+//             bOpen = true;
+//             return con.callAPIOnce(service, functionName, data).then(
+//                 data => {
+//                     if (con && typeof con.disconnect == "function") {
+//                         con.disconnect();
+//                     }
+//                     return data;
+//                 },
+//                 error => {
+//                     if (con && typeof con.disconnect == "function") {
+//                         con.disconnect();
+//                     }
+//                     if (error.status) {
+//                         return Q.reject(error);
+//                     }
+//                     else {
+//                         return Q.reject({
+//                             status: 400,
+//                             message: "SMS is not available",
+//                             error: error
+//                         });
+//                     }
+//                 }
+//             );
+//         }
+//     ).then(deferred.resolve, deferred.reject);
+//     return deferred.promise;
+// };
 
 const smsAPI = {
     /*
