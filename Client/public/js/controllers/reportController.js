91502438b5b5c9b8119ec8e9ea250def4cf6597e
marktai <mark@snsoft.my>
2017-10-20 09:50:04 +0800

    modify the field display

--- Client/public/js/controllers/reportController.js
+++ Client/public/js/controllers/reportController.js
@@ -1485,8 +1485,9 @@ define(['js/app'], function (myApp) {
                 columns: [
                     {
                         "title": $translate('proposalId'),
-                        "data": "proposalId",
+                        data: "proposalId",
                         render: function (data, type, row) {
+                          data = String(data);
                           return '<a ng-click="vm.showProposalModal2('+data+')">'+data+'</a>';
                         }
                     },
