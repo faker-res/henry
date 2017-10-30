3be095278f50e43119531ce1a297fc800c5bbc33
yewhan <yewhan@snsoft.my>
2017-10-30 09:29:30 +0800

    phone num filter - copy phone number list

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -13031,6 +13031,29 @@ define(['js/app'], function (myApp) {
                 vm.diffPhoneList = '';
             };
 
+            // copy phone number list
+            vm.copyToClipboard = function (elementId) {
+                vm.copyHere = false;
+
+                // Create an auxiliary hidden input
+                var aux = document.createElement("input");
+
+                // Get the text from the element passed into the input
+                aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+
+                // Append the aux input to the body
+                document.body.appendChild(aux);
+
+                // Highlight the content
+                aux.select();
+
+                // Execute the copy command
+                document.execCommand("copy");
+
+                // Remove the input from the body
+                document.body.removeChild(aux);
+            };
+
             // player level codes==============end===============================
 
             // partner level codes==============start===============================
