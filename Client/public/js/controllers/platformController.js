5af5e7781656a5fc129a8cb627769c8c5cfb9cb2
yewhan <yewhan@snsoft.my>
2017-11-02 14:30:33 +0800

    phone num filter - filter all platform ui

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -13126,6 +13126,7 @@ define(['js/app'], function (myApp) {
                 vm.inputNewPhoneNum = [];
                 vm.phoneNumCSVResult = false;
                 vm.phoneNumTXTResult = false;
+                vm.filterAllPlatform = false;
             };
 
             // compare a new list pf phone numbers with existing player info database
