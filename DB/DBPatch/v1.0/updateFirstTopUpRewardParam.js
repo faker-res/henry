ae52bb686e77d91227387db5d330e0c1d8697e2f
yeekit <yeekit@snsoft.my>
2017-11-15 16:31:07 +0800

    fix reward index conflict bug

--- DB/DBPatch/v1.0/updateFirstTopUpRewardParam.js
+++ DB/DBPatch/v1.0/updateFirstTopUpRewardParam.js
@@ -793,7 +793,7 @@ db.rewardParam.update({
             },
             customCond: {
                 rewardAppearPeriod: {
-                    index: 22,
+                    index: 26,
                     type: "datetimePeriod",
                     des: "Period show reward",
                     value: {}
