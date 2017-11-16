bd946d2d7cbfd8c1ba941e49bfbf47b71816dd07
linhao <mars@snsoft.my>
2017-11-16 12:03:34 +0800

    set default value to 0 before assigning  value to the object key

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -11589,6 +11589,11 @@ let dbPlayerInfo = {
                 let bonusDetails = {};
                 if(platformDetails){
                     if(playerDetails){
+                        result.freeTimes = 0;
+                        result.serviceCharge = 0;
+                        result.currentFreeAmount = 0;
+                        result.freeAmount = 0;
+
                         if(platformDetails.bonusSetting){
                             for(let x in platformDetails.bonusSetting){
                                 if(platformDetails.bonusSetting[x].name == playerDetails.playerLevel.name){
