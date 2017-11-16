1fa2c2431abdce78bb967cdf8c5a36351ea2c6d5
linhao <mars@snsoft.my>
2017-11-16 11:49:53 +0800

    replace data.playerId with conn.playerId

--- Server/serviceImplements/client/PlayerServiceImplement.js
+++ Server/serviceImplements/client/PlayerServiceImplement.js
@@ -980,9 +980,10 @@ let PlayerServiceImplement = function () {
     };
 
     this.getWithdrawalInfo.onRequest = function (wsFunc, conn, data) {
-        var isValidData = Boolean(data.playerId && data.platformId);
-        WebSocketUtil.performAction(conn, wsFunc, data, dbPlayerInfo.getWithdrawalInfo, [data.platformId, data.playerId], isValidData, false, false, true);
+        var isValidData = Boolean(conn.playerId && data.platformId);
+        WebSocketUtil.performAction(conn, wsFunc, data, dbPlayerInfo.getWithdrawalInfo, [data.platformId, conn.playerId], isValidData, false, false, true);
     };
+
 
 };
 var proto = PlayerServiceImplement.prototype = Object.create(PlayerService.prototype);
