3cf703087265c4e2b135423e60687d66c7df65ae
vincentsnsoft <vincent@snsoft.my>
2017-11-17 22:49:08 +0800

    update wechat topup rejection function

--- Server/modules/proposalExecutor.js
+++ Server/modules/proposalExecutor.js
@@ -2364,9 +2364,10 @@ var proposalExecutor = {
                 //         }
                 //     );
                 // }
-                pmsAPI.payment_requestCancellationPayOrder({proposalId: proposalData.proposalId}).then(
-                    deferred.resolve, deferred.reject
-                );
+                // pmsAPI.payment_requestCancellationPayOrder({proposalId: proposalData.proposalId}).then(
+                //     deferred.resolve, deferred.reject
+                // );
+                deferred.resolve("Proposal is rejected")
             },
 
             /**
