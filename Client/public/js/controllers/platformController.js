9a361513fc1c8fa762c5cb37fbd828617107ec73
zheming <zheming@snsoft.my>
2017-11-24 14:43:27 +0800

    remove console log

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -14054,7 +14054,6 @@ define(['js/app'], function (myApp) {
 
                 if (vm.showRewardTypeData.isGrouped === true) {
                     // Set condition
-                    console.log("WLAOWHOLE",vm.rewardMainCondition);
                     Object.keys(vm.rewardMainCondition).forEach(e => {
                         if (vm.rewardMainCondition[e].value !== undefined) {
                             let condName = vm.rewardMainCondition[e].name;
