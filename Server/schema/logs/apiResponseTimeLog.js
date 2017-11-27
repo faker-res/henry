35d5034dfc2d1a942f43525202d11cb193016e73
vincentsnsoft <vincent@snsoft.my>
2017-11-27 11:45:38 +0800

    add index for api response time log

--- Server/schema/logs/apiResponseTimeLog.js
+++ Server/schema/logs/apiResponseTimeLog.js
@@ -4,17 +4,21 @@ var Schema = mongoose.Schema;
 // Player credit change log
 var apiResponseTimeLogSchema = new Schema ({
     //api service name
-    service: {type: String},
+    service: {type: String, index: true},
     //api function name
-    functionName: {type: String},
+    functionName: {type: String, index: true},
     // request Data
     requestData: JSON,
     // response Data
     responseData: JSON,
     // Date of action
-    createTime: {type: Date, default: Date.now},
+    createTime: {type: Date, default: Date.now, index: true},
     // response time
     responseTime: Number,
 });
 
+apiResponseTimeLogSchema.index({service: 1, functionName: 1, createTime: 1});
+
 module.exports = apiResponseTimeLogSchema;
+
+
