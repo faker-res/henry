3991f3ec84405c00f379ff703c5cd58d10340aac
marktai <mark@snsoft.my>
2017-12-26 17:56:19 +0800

    #3222: hotfix for a bunch of error message

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -10721,7 +10721,7 @@ define(['js/app'], function (myApp) {
                             item.currentAmount$ = item.data.currentAmount;
 
                         })
-                        vm.drawRewardTaskTable(true, result, 0, summary, 0);
+                        vm.drawRewardTaskTable(true, result, 0, summary, 0, 0);
                         // vm.drawRewardTaskTable(true, data.data, size, summary, topUpAmountSum);
                         vm.curRewardTask = data;
                     })
