d0936f9dbffac93f0dcc5e7958597bc75972182e
YeeHui <yeehui@snsoft.my>
2017-12-27 11:26:41 +0800

    #3484: 前端Slider- 显示配置修改

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -277,16 +277,16 @@ define(['js/app'], function (myApp) {
                     buttonName: "activityBtn1",
                     url:"",
                     hyperLink: "",
-                    css:"position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                    hoverCss: ":hover{width:500px;}"
+                    css:"position:absolute; width: auto; height: auto; top:87%; left: 20%",
+                    hoverCss: ":hover{filter: contrast(200%);}"
                 },
                 {
                     buttonNo: 2,
                     buttonName: "activityBtn2",
                     url:"",
                     hyperLink: "",
-                    css: "position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                    hoverCss: ":hover{width:500px;}"
+                    css: "position:absolute; width: auto; height: auto; top:87%; left: 70%",
+                    hoverCss: ":hover{filter: contrast(200%);}"
                 }
             ];
 
@@ -308,16 +308,16 @@ define(['js/app'], function (myApp) {
                     buttonName: "activityBtn1",
                     url:"",
                     hyperLink: "",
-                    css:"position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                    hoverCss: ":hover{width:500px;}"
+                    css:"position:absolute; width: auto; height: auto; top:87%; left: 20%",
+                    hoverCss: ":hover{filter: contrast(200%);}"
                 },
                 {
                     buttonNo: 2,
                     buttonName: "activityBtn2",
                     url:"",
                     hyperLink: "",
-                    css: "position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                    hoverCss: ":hover{width:500px;}"
+                    css: "position:absolute; width: auto; height: auto; top:87%; left: 70%",
+                    hoverCss: ":hover{filter: contrast(200%);}"
                 }
             ];
 
@@ -21307,7 +21307,7 @@ define(['js/app'], function (myApp) {
                         $scope.safeApply();
                     }
                     else {
-                        Q.reject('Advertisement List is not found.');}
+                        Q.reject('Advertisement list is not found.');}
                 });
 
             };
@@ -21339,6 +21339,11 @@ define(['js/app'], function (myApp) {
                     document.getElementsByTagName('head')[0].appendChild(vm.hoverStyle);
                     $scope.safeApply();
                 }
+            };
+
+            vm.clearStyle = function (){
+                document.getElementsByTagName('head')[0].removeChild(vm.hoverStyle);
+                $scope.safeApply();
             };
 
             vm.advSettingUpdate = function(elem, subject){
@@ -21460,8 +21465,8 @@ define(['js/app'], function (myApp) {
                         buttonName: 'activityBtn' + buttonNo,
                         url: '',
                         hyperLink: '',
-                        css:"position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                        hoverCss: ":hover{width:500px;}"
+                        css:"position:absolute; width: auto; height: auto; top:50%; left: 50%",
+                        hoverCss: ":hover{filter: contrast(200%);}"
                     }
                 );
 
@@ -21539,16 +21544,16 @@ define(['js/app'], function (myApp) {
                         buttonName: "activityBtn1",
                         url:"",
                         hyperLink: "",
-                        css:"position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                        hoverCss: ":hover{width:500px;}"
+                        css:"position:absolute; width: auto; height: auto; top:87%; left: 20%",
+                        hoverCss: ":hover{filter: contrast(200%);}"
                     },
                     {
                         buttonNo: 2,
                         buttonName: "activityBtn2",
                         url:"",
                         hyperLink: "",
-                        css: "position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                        hoverCss: ":hover{width:500px;}"
+                        css: "position:absolute; width: auto; height: auto; top:87%; left: 70%",
+                        hoverCss: ":hover{filter: contrast(200%);}"
                     }
                 ];
 
@@ -21822,8 +21827,8 @@ define(['js/app'], function (myApp) {
                         buttonName: 'activityBtn' + buttonNo,
                         url: '',
                         hyperLink: '',
-                        css:"position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                        hoverCss: ":hover{width:500px;}"
+                        css:"position:absolute; width: auto; height: auto; top:87%; left: 70%",
+                        hoverCss:":hover{filter: contrast(200%);}"
                     }
                 );
 
@@ -21898,16 +21903,16 @@ define(['js/app'], function (myApp) {
                         buttonName: "activityBtn1",
                         url:"",
                         hyperLink: "",
-                        css:"position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                        hoverCss: ":hover{width:500px;}"
+                        css:"position:absolute; width: auto; height: auto; top:87%; left: 20%",
+                        hoverCss: ":hover{filter: contrast(200%);}"
                     },
                     {
                         buttonNo: 2,
                         buttonName: "activityBtn2",
                         url:"",
                         hyperLink: "",
-                        css: "position:absolute; width: 195px; height: 80px; top:150px; left: 500px",
-                        hoverCss: ":hover{width:500px;}"
+                        css: "position:absolute; width: auto; height: auto; top:87%; left: 70%",
+                        hoverCss: ":hover{filter: contrast(200%);}"
                     }
                 ];
 
