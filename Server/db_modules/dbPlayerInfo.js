283c6219e32e043efdc34eac7833c80db5f3d557
huat <huat@snsoft.my>
2018-01-05 10:49:24 +0800

    补单功能 test fix
    - 原因是因为找record时限制100个，可是在100个内找不到玩家的transfer record，所以就无法修复。

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -9969,10 +9969,18 @@ let dbPlayerInfo = {
         )
     },
 
-    getPlayerTransferErrorLog: function (playerObjId) {
-        return dbconfig.collection_playerCreditTransferLog.find({
-            playerObjId: playerObjId, bUsed: {$ne: true}    //status: constPlayerCreditTransferStatus.FAIL
-        }).sort({"createTime": -1}).limit(constSystemParam.MAX_RECORD_NUM);
+    getPlayerTransferErrorLog: function (playerObjId, createTime) {
+        let query = {
+            playerObjId: playerObjId,
+            bUsed: {$ne: true},
+            // status: constPlayerCreditTransferStatus.FAIL
+        };
+
+        if (createTime) {
+            query.createTime = createTime;
+        }
+
+        return dbconfig.collection_playerCreditTransferLog.find(query).sort({"createTime": -1}).limit(constSystemParam.MAX_RECORD_NUM);
     },
 
     verifyPlayerPhoneNumber: function (playerObjId, phoneNumber) {
