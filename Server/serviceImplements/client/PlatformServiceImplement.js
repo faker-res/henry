3bf1130d5feb7a7998afae837651a59c81a28dcd
vincentsnsoft <vincent@snsoft.my>
2018-01-18 15:02:19 +0800

    update variable define

--- Server/serviceImplements/client/PlatformServiceImplement.js
+++ Server/serviceImplements/client/PlatformServiceImplement.js
@@ -1,7 +1,7 @@
-var WebSocketUtil = require("./../../server_common/WebSocketUtil");
-var PlatformService = require("./../../services/client/ClientServices").PlatformService;
-var dbPlatform = require('./../../db_modules/dbPlatform');
-var dbPlatformAnnouncement = require("../../db_modules/dbPlatformAnnouncement");
+const WebSocketUtil = require("./../../server_common/WebSocketUtil");
+const PlatformService = require("./../../services/client/ClientServices").PlatformService;
+const dbPlatform = require('./../../db_modules/dbPlatform');
+const dbPlatformAnnouncement = require("../../db_modules/dbPlatformAnnouncement");
 const dbUtility = require('./../../modules/dbutility');
 
 var PlatformServiceImplement = function () {
@@ -9,14 +9,14 @@ var PlatformServiceImplement = function () {
 
     this.getPlatformDetails.expectsData = 'platformId: String';
     this.getPlatformDetails.onRequest = function (wsFunc, conn, data) {
-        var isValidData = Boolean(data && data.platformId);
+        let isValidData = Boolean(data && data.platformId);
         data = data || {};
         WebSocketUtil.performAction(conn, wsFunc, data, dbPlatform.getPlatform, [{platformId:data.platformId}], isValidData, null, null, true);
     };
 
     this.getPlatformAnnouncements.expectsData = 'platformId: String';
     this.getPlatformAnnouncements.onRequest = function (wsFunc, conn, data) {
-        var isValidData = Boolean(data && data.platformId);
+        let isValidData = Boolean(data && data.platformId);
         data = data || {};
         WebSocketUtil.performAction(conn, wsFunc, data, dbPlatformAnnouncement.getPlatformAnnouncementsByPlatformId, [{platformId: data.platformId}], isValidData, null, null, true);
     };
@@ -32,15 +32,14 @@ var PlatformServiceImplement = function () {
 
     this.getLiveStream.expectsData = '';
     this.getLiveStream.onRequest = function (wsFunc, conn, data) {
-        var isValidData = true;
+        let isValidData = true;
         data = data || {};
         WebSocketUtil.performAction(conn, wsFunc, data, dbPlatform.getLiveStream, [conn.playerObjId], isValidData, null, null, true);
     };
 
     this.playerPhoneChat.onRequest = function (wsFunc, conn, data) {
-        var isValidData = true;
         data = data || {};
-        var isValidData = Boolean(data && data.phone && data.captcha && data.random);
+        let isValidData = Boolean(data && data.phone && data.captcha && data.random);
         WebSocketUtil.performAction(conn, wsFunc, data, dbPlatform.playerPhoneChat, [data.phone, data.captcha, data.random], isValidData, null, null, true);
     };
 
