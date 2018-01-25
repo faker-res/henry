90b7e5151471e512a3e16228636807c5f32cc331
zheming <zheming@snsoft.my>
2018-01-25 11:02:59 +0800

    #52-36 (电销开户报表）的总共 N 条记录

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -11445,7 +11445,7 @@ let dbPlayerInfo = {
                 outputResult = query.csPromoteWay && query.csPromoteWay.length > 0 ? outputResult.filter(e => query.csPromoteWay.indexOf(e.csPromoteWay) >= 0) : outputResult;
                 outputResult = query.admins && query.admins.length > 0 ? outputResult.filter(e => query.admins.indexOf(e.csOfficer) >= 0) : outputResult;
 
-                return {size: result.length, data: outputResult};
+                return {size: outputResult.length, data: outputResult};
             }
         );
     },
