e8a3afc3f4addb8a8eeb5ab3e079e8682442dc84
jingyi <jykhow@snsoft.my>
2018-01-29 16:47:01 +0800

    修改提案报表的状态

--- Client/public/js/controllers/reportController.js
+++ Client/public/js/controllers/reportController.js
@@ -34,7 +34,6 @@ define(['js/app'], function (myApp) {
             PREPENDING: "PrePending",
             PENDING: "Pending",
             PROCESSING: "Processing",
-            APPROVED: "approved",
             SUCCESS: "Success",
             FAIL: "Fail",
             CANCEL: "Cancel",
@@ -4461,7 +4460,7 @@ define(['js/app'], function (myApp) {
                 rewardTypeName: newproposalQuery.rewardTypeName,
                 promoTypeName: newproposalQuery.promoTypeName,
                 platformId: vm.curPlatformId,
-                status: newproposalQuery.status == "approved" ? "Approved" : newproposalQuery.status,
+                status: newproposalQuery.status,
                 relatedAccount: newproposalQuery.relatedAccount,
                 index: newSearch ? 0 : (newproposalQuery.index || 0),
                 limit: newproposalQuery.limit,
@@ -4495,7 +4494,7 @@ define(['js/app'], function (myApp) {
                     if (item.data && item.data.remark) {
                         item.remark$ = item.data.remark;
                     }
-                    item.status$ = $translate(vm.getStatusStrfromRow(item) == "Approved" ? "approved" : vm.getStatusStrfromRow(item)) ;
+                    item.status$ = $translate(vm.getStatusStrfromRow(item));
 
                     return item;
                 })
