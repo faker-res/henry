1db17c2e1a4e6cc444fe655cc8ce97facd7aa1e9
yeekit <yeekit@snsoft.my>
2018-02-01 14:43:22 +0800

    提案细节的金额都只显示2位小数

--- Client/public/js/controllers/operationController.js
+++ Client/public/js/controllers/operationController.js
@@ -695,6 +695,8 @@ define(['js/app'], function (myApp) {
                 result = result.join(',');
             } else if ((fieldName.indexOf('time') > -1 || fieldName.indexOf('Time') > -1) && val) {
                 result = utilService.getFormatTime(val);
+            } else if ((fieldName.indexOf('amount') > -1 || fieldName.indexOf('Amount') > -1) && val) {
+                result = Number.isFinite(parseFloat(val)) ? parseFloat(val).toFixed(2) : val;
             } else if (fieldName == 'bankAccountType') {
                 switch (parseInt(val)) {
                     case 1:
