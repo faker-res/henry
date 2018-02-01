80e78feae1ef1db6499354fa23750f4970c01eb3
jingyi <jykhow@snsoft.my>
2018-02-01 17:49:55 +0800

    55-1 （基础数据->有效/活跃玩家) 重新定义活跃玩家

--- Server/schema/partnerLevelConfig.js
+++ Server/schema/partnerLevelConfig.js
@@ -12,7 +12,32 @@ var partnerLevelConfigSchema = new Schema({
     activePlayerTopUpTimes: {type: Number, required: true},
     activePlayerTopUpAmount: {type: Number, required: true, default: 0},
     activePlayerConsumptionTimes: {type: Number, required: true},
-    activePlayerValue: {type: Number, required: true, default: 0}
+    activePlayerValue: {type: Number, required: true, default: 0},
+    // This is how we decide if a player is active by daily
+    dailyActivePlayerTopUpTimes: {type: Number, required: true, default: 0},
+    dailyActivePlayerTopUpAmount: {type: Number, required: true, default: 0},
+    dailyActivePlayerConsumptionTimes: {type: Number, required: true, default: 0},
+    dailyActivePlayerValue: {type: Number, required: true, default: 0},
+    // This is how we decide if a player is active by weekly
+    weeklyActivePlayerTopUpTimes: {type: Number, required: true, default: 0},
+    weeklyActivePlayerTopUpAmount: {type: Number, required: true, default: 0},
+    weeklyActivePlayerConsumptionTimes: {type: Number, required: true, default: 0},
+    weeklyActivePlayerValue: {type: Number, required: true, default: 0},
+    // This is how we decide if a player is active by half month
+    halfMonthActivePlayerTopUpTimes: {type: Number, required: true, default: 0},
+    halfMonthActivePlayerTopUpAmount: {type: Number, required: true, default: 0},
+    halfMonthActivePlayerConsumptionTimes: {type: Number, required: true, default: 0},
+    halfMonthActivePlayerValue: {type: Number, required: true, default: 0},
+    // This is how we decide if a player is active by monthly
+    monthlyActivePlayerTopUpTimes: {type: Number, required: true, default: 0},
+    monthlyActivePlayerTopUpAmount: {type: Number, required: true, default: 0},
+    monthlyActivePlayerConsumptionTimes: {type: Number, required: true, default: 0},
+    monthlyActivePlayerValue: {type: Number, required: true, default: 0},
+    // This is how we decide if a player is active by season
+    seasonActivePlayerTopUpTimes: {type: Number, required: true, default: 0},
+    seasonActivePlayerTopUpAmount: {type: Number, required: true, default: 0},
+    seasonActivePlayerConsumptionTimes: {type: Number, required: true, default: 0},
+    seasonActivePlayerValue: {type: Number, required: true, default: 0}
 });
 
 module.exports = partnerLevelConfigSchema;
