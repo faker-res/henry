f226598bfe025556c016900894df55758e715794
huat <huat@snsoft.my>
2018-02-08 16:40:27 +0800

    更新玩家提款时 检查提案要过滤掉无效提案

--- Server/dataPatch/updatePlayerBonusAmountSum.js
+++ Server/dataPatch/updatePlayerBonusAmountSum.js
@@ -1,14 +1,14 @@
-const dbconfig = require("../modules/dbproperties")
+const dbconfig = require("../modules/dbproperties");
+const constProposalStatus = require("../const/constProposalStatus");
 
 const playerCursor = dbconfig.collection_players.find({}).cursor();
 let i = 0;
 playerCursor.eachAsync(
     player => {
-        return dbconfig.collection_proposal.find({mainType: "PlayerBonus", "data.playerId": player.playerId}).count().then(
+        return dbconfig.collection_proposal.find({mainType: "PlayerBonus", "data.playerId": player.playerId, "status": {$in: [constProposalStatus.SUCCESS, constProposalStatus.APPROVED]}}).count().then(
             withdrawCount => {
                 if (withdrawCount) {
                     console.log('withdrawCount', withdrawCount)
-
                 }
                 return dbconfig.collection_players.update({
                     _id: player._id,
