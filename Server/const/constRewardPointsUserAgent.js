f871992866480acdee6f89f72f3a69228b91d665
jingyi <jykhow@snsoft.my>
2018-03-07 17:25:23 +0800

    57-17 新系统API - getMissonList 获取任务信息

---
+++ Server/const/constRewardPointsUserAgent.js
@@ -0,0 +1,15 @@
+/*
+ * User Agent
+ */
+const constRewardPointsUserAgent = {
+    "-1": "全选",
+    "0": "后台",
+    "1": "WEB玩家",
+    "2": "WEB代理",
+    "3": "H5玩家",
+    "4": "H5代理",
+    "5": "APP玩家",
+    "6": "APP代理"
+};
+
+module.exports = constRewardPointsUserAgent;
