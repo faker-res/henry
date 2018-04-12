d22740ea76c1a6e02ab15b07a64997741f936dba
zheming <zheming@snsoft.my>
2018-04-12 15:50:25 +0800

    replaceMailKeywords bug fix

--- Server/db_modules/dbDXMission.js
+++ Server/db_modules/dbDXMission.js
@@ -380,6 +380,8 @@ function replaceMailKeywords(str, dxMission, dxPhone, player, providerGroupName)
     str = str.replace ('{{creditAmount}}', dxMission.creditAmount);
     str = str.replace ('{{providerGroup}}', providerGroupName);
     str = str.replace ('{{requiredConsumption}}', dxMission.requiredConsumption);
+
+    return str;
 }
 
 function generateDXCode(dxMission, platformId, tries) {
