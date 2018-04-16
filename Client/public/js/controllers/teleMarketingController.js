ec96b35e870d2520fdcb365ac62db13dba83f4ef
yewhan <yewhan@snsoft.my>
2018-04-16 15:35:17 +0800

    result status after import phone number

--- Client/public/js/controllers/teleMarketingController.js
+++ Client/public/js/controllers/teleMarketingController.js
@@ -698,6 +698,7 @@ define(['js/app'], function (myApp) {
                 };
                 vm.allDxMission = [];
                 vm.getAllDxMission();
+                vm.importPhoneResult = '';
             };
 
             vm.getAllDxMission = function () {
@@ -718,6 +719,13 @@ define(['js/app'], function (myApp) {
                 };
 
                 socketService.$socket($scope.AppSocket, 'importDiffPhoneNum', sendData, function (data) {
+                    if (data.success && data.data) {
+                        //display success
+                        vm.importPhoneResult = 'IMPORT_SUCCESS';
+                    } else {
+                        //display error
+                        vm.importPhoneResult = 'IMPORT_FAIL';
+                    }
 
                     $scope.safeApply();
                 });
@@ -842,6 +850,7 @@ define(['js/app'], function (myApp) {
                 vm.samePhoneTotalTXT = '';
                 vm.diffPhoneTotalTXT = '';
                 vm.selectedDxMission = '';
+                vm.importPhoneResult = '';
             };
             /****************** TXT - end ******************/
 
@@ -875,6 +884,7 @@ define(['js/app'], function (myApp) {
                 vm.samePhoneList = '';
                 vm.diffPhoneList = '';
                 vm.selectedDxMission = '';
+                vm.importPhoneResult = '';
             };
 
             // copy phone number list
@@ -1028,6 +1038,7 @@ define(['js/app'], function (myApp) {
                 vm.diffPhoneTotalXLS = '';
                 vm.phoneNumXLSResult = false;
                 vm.selectedDxMission = '';
+                vm.importPhoneResult = '';
             };
             /****************** XLS - end ******************/
             // phone number filter codes==============end===============================
