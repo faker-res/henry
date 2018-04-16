0b6f30473e671fc1ea427118cd3563de4336464a
yewhan <yewhan@snsoft.my>
2018-04-16 11:27:31 +0800

    filter out empty strings (due to extra comma)

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -21489,6 +21489,7 @@ define(['js/app'], function (myApp) {
             // upload phone file: txt
             vm.uploadPhoneFileTXT = function (content) {
                 vm.arrayPhoneTXT = content.split(/,|, /).map((item) => item.trim());
+                vm.arrayPhoneTXT = vm.arrayPhoneTXT.filter(Boolean); //filter out empty strings (due to extra comma)
 
                 let sendData = {
                     filterAllPlatform: vm.filterAllPlatform,
@@ -21566,6 +21567,7 @@ define(['js/app'], function (myApp) {
             // generate a new list of phone numbers without existing player phone number
             vm.comparePhoneNum = function () {
                 vm.arrayInputPhone = vm.inputNewPhoneNum.split(/,|, /).map((item) => item.trim());
+                vm.arrayInputPhone = vm.arrayInputPhone.filter(Boolean); //filter out empty strings (due to extra comma)
 
                 let sendData = {
                     filterAllPlatform: vm.filterAllPlatform,
