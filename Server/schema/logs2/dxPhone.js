1da4120affdc041bdebb47ae509ce8ffaffe2d6e
linhao <mars@snsoft.my>
2018-04-19 18:35:38 +0800

    set index for dxMission and dxPhone schema

--- Server/schema/logs2/dxPhone.js
+++ Server/schema/logs2/dxPhone.js
@@ -5,9 +5,9 @@ var dxPhone = new Schema({
     // platform
     platform: {type: Schema.ObjectId, ref: 'platform', index: true, required: true},
     // mission name
-    phoneNumber: {type: String, required: true, index: true},
+    phoneNumber: {type: String, required: true},
     // relevant dx mission object Id
-    dxMission: {type: Schema.ObjectId, ref: 'dxMission', required: true, index: true},
+    dxMission: {type: Schema.ObjectId, ref: 'dxMission', required: true},
     // code use to retrieve account
     code: {type: String, index: true, required: true},
     // create time
@@ -17,8 +17,9 @@ var dxPhone = new Schema({
     // generated url
     url: {type: String},
     // player info after creation
-    playerObjId: {type: Schema.ObjectId, ref: 'player', index: true}
+    playerObjId: {type: Schema.ObjectId, ref: 'player'}
 });
 
+dxPhone.index({ platform: 1, dxMission: 1, playerObjId: 1, createTime: 1 }, {unique: true});
 
 module.exports = dxPhone;
