62d833facb8367b309fc0c50bc4144280a56ded6
ricco <ricco@snsoft.my>
2018-04-20 10:59:44 +0800

    commonService

---
+++ Client/public/js/services/commonService.js
@@ -0,0 +1,31 @@
+'use strict';
+
+define([], () => {
+    let commonService = function () {
+        this.cookieTokenKey = 'sinonet-management-token';
+        this.cookieAdminIdKey = 'sinonet-management-adminId';
+        this.cookieAdminNameKey = 'sinonet-management-adminName';
+        this.cookiePolicyKey = 'sinonet-management-role';
+        this.cookieDepartmentKey = 'sinonet-management-departments';
+        this.cookieLanguageKey = 'sinonet-management-language';
+        this.cookiePlatformKey = 'sinonet-management-platform';
+
+        let self = this;
+
+        this.$get = function () {
+            return this;
+        };
+
+        this.updatePageTile = ($translate, pageName, tabName) => {
+            window.document.title = $translate(pageName) + "->" + $translate(tabName);
+            $(document).one('shown.bs.tab', function (e) {
+                $(document).trigger('resize');
+            });
+        };
+    };
+
+    let commonServiceApp = angular.module('commonService', []);
+
+    //Must be a provider since it will be injected into module.config()
+    commonServiceApp.provider('commonService', commonService);
+});
