b480e8117b900703f2aae80285933a7ba1128f10
ricco <ricco@snsoft.my>
2018-04-21 15:34:24 +0800

    60-15-2 (Partial)

--- Server/schema/player.js
+++ Server/schema/player.js
@@ -312,6 +312,7 @@ var playerSchema = new Schema({
 
 //record is unique by name and platform
 playerSchema.index({name: 1, platform: 1});
+playerSchema.index({platform: 1, partner: 1});
 
 playerSchema.pre('save', counterManager.incrementCounterAndSetPropertyIfNew('playerId'));
 
