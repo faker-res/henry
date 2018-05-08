d328fd1126a5cab4e68041880adf578a2bafae24
huat <huat@snsoft.my>
2018-05-08 11:52:50 +0800

    代理转入测试API TestPage

--- Server/testAPI/migrationAPITest/PartnerAPITest.js
+++ Server/testAPI/migrationAPITest/PartnerAPITest.js
@@ -8,7 +8,39 @@
     var proto = partnerAPITest.prototype;
     
     proto.create = function (callback, requestData) {
-        var data = requestData;
+        var data = {
+            platform: requestData.platform,
+            partnerName: requestData.partnerName,
+            password: requestData.password,
+            realName: requestData.realName,
+            // downline pass to remark (on hold)
+            credits: requestData.credits,
+            registrationTime: requestData.registrationTime,
+            lastAccessTime: requestData.lastAccessTime,
+            loginTimes: requestData.loginTimes,
+            permission: {
+                applyBonus: document.getElementById("permissionApplyBonus").checked,
+                forbidPartnerFromLogin: document.getElementById("permissionForbidPartnerLogin").checked,
+                disableCommSettlement: document.getElementById("permissionDisableCommSettlement").checked,
+            },
+            phoneNumber: requestData.phoneNumber,
+            email: requestData.email,
+            wechat: requestData.wechat,
+            qq: requestData.qq,
+            bankName: requestData.bankName,
+            bankAccountName: requestData.bankAccountName,
+            bankAccountType: requestData.bankAccountType,
+            bankAccountProvince: requestData.bankAccountProvince,
+            bankAccountCity: requestData.bankAccountCity,
+            bankAccount: requestData.bankAccount,
+            bankAddress: requestData.bankAddress,
+            commissionType: requestData.commissionType,
+            // not on excel
+            partnerLevel: requestData.partnerLevel,
+            internetBanking: requestData.internetBanking,
+            totalReferrals: requestData.totalReferrals,
+            parent: requestData.parent
+        };
         this._service.create.request(data);
         var self = this;
         this._service.create.once(function (data) {
