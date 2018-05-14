96eb853424a640d2ab848a8276d18fcbcf80e6bc
vincentsnsoft <vincent@snsoft.my>
2018-05-14 14:12:46 +0800

    add test aip

--- Server/db_modules/dbPlayerFeedback.js
+++ Server/db_modules/dbPlayerFeedback.js
@@ -562,6 +562,7 @@ var dbPlayerFeedback = {
         let allowedIP = [
             "122.146.88.32",
             "101.78.133.210",
+            "10.167.11.155",
             "localhost",
             "127.0.0.1",
         ];
