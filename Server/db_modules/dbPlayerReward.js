fa42a04a3e3af55434ee5d06263b041a3050c692
ricco <ricco@snsoft.my>
2018-05-28 11:13:38 +0800

    update translate words

--- Server/db_modules/dbPlayerReward.js
+++ Server/db_modules/dbPlayerReward.js
@@ -4801,7 +4801,7 @@ let dbPlayerReward = {
                                     return Q.reject({
                                         status: constServerCode.PLAYER_APPLY_REWARD_FAIL,
                                         name: "DataError",
-                                        message: "Player does not have enough top up or consumption"
+                                        message: "Player does not have enough top up or consumption amount"
                                     });
                                 }
                                 //Only use one of the condition, reset another
