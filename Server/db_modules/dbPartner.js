2dac594390f9e0f3aa02a45260eb3c1fdc0254d6
huatsnsoft <huat@snsoft.my>
2018-06-08 15:58:21 +0800

    add null check

--- Server/db_modules/dbPartner.js
+++ Server/db_modules/dbPartner.js
@@ -6728,6 +6728,7 @@ let dbPartner = {
                         }
                     } else {
                         for (let i = 0; i < commissionData.length; i++) {
+                            if (!commissionData[i].provider) continue;
                             let commissionObj = {
                                 providerGroupId: commissionData[i].provider.providerGroupId ? commissionData[i].provider.providerGroupId : "",
                                 providerGroupName: commissionData[i].provider.name ? commissionData[i].provider.name : ""
