921bff88c605e30a1a49adb1b62c3449d3912459
jykhow <jykhow@snsoft.my>
2018-06-11 10:43:35 +0800

    Fix 代理API - SMS发送记录账号显示错误 Bug

--- Server/serviceImplements/client/PartnerServiceImplement.js
+++ Server/serviceImplements/client/PartnerServiceImplement.js
@@ -394,10 +394,8 @@ var PartnerServiceImplement = function () {
         conn.captchaCode = null;
         // wsFunc.response(conn, {status: constServerCode.SUCCESS, data: randomCode}, data);
         let inputDevice = dbUtility.getInputDevice(conn.upgradeReq.headers['user-agent'], true);
-        if (conn.isAuth && conn.partnerObjId && !data.name) {
-            data.name = conn.partnerObjId;
-        }
-        WebSocketUtil.performAction(conn, wsFunc, data, dbPlayerMail.sendVerificationCodeToNumber, [conn.phoneNumber, conn.smsCode, data.platformId, captchaValidation, data.purpose, inputDevice, data.name, null, true], isValidData, false, false, true);
+
+        WebSocketUtil.performAction(conn, wsFunc, data, dbPlayerMail.sendVerificationCodeToNumber, [conn.phoneNumber, conn.smsCode, data.platformId, captchaValidation, data.purpose, inputDevice, data.name, null, true, conn.partnerObjId], isValidData, false, false, true);
     };
 
     this.updatePhoneNumberWithSMS.expectsData = 'partnerId: String, phoneNumber: Number';
