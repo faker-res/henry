695c32ad2278274d3eec60bd935b0073c31725bc
mars <mars@snsoft.my>
2018-06-13 15:53:28 +0800

    代理API修复 - 当period = 1 时，可选择传送circleTimes 参数或startTime,endTime参数

--- Server/db_modules/dbPartner.js
+++ Server/db_modules/dbPartner.js
@@ -7400,7 +7400,7 @@ let dbPartner = {
                 let nextPeriod = getCurrentCommissionPeriod(periodCycle);
                 let outputProms = [];
 
-                if(periodCycle == 1){
+                if(periodCycle == 1 && !circleTimes){
                     startDate = new Date(startDate);
                     endDate = new Date(endDate);
 
@@ -7461,7 +7461,7 @@ let dbPartner = {
                 let nextPeriod = getCurrentCommissionPeriod(periodCycle);
                 let outputProms = [];
 
-                if(periodCycle == 1){
+                if(periodCycle == 1 && !circleTimes){
                     startDate = new Date(startDate);
                     endDate = new Date(endDate);
 
@@ -7551,7 +7551,7 @@ let dbPartner = {
                 let nextPeriod = getCurrentCommissionPeriod(periodCycle);
                 let outputProms = [];
 
-                if(periodCycle == 1){
+                if(periodCycle == 1 && !circleTimes){
                     startDate = new Date(startDate);
                     endDate = new Date(endDate);
 
@@ -7779,7 +7779,7 @@ let dbPartner = {
                 let outputProms = [];
                 let providerGroups = providerGroup ? [providerGroup] : null;
 
-                if(periodCycle == 1){
+                if(periodCycle == 1 && !circleTimes){
                     startDate = new Date(startDate);
                     endDate = new Date(endDate);
 
@@ -7873,7 +7873,7 @@ let dbPartner = {
 
                 let nextPeriod = getCurrentCommissionPeriod(periodCycle);
                 let outputProms = [];
-                if(periodCycle == 1){
+                if(periodCycle == 1 && !circleTimes){
                     startDate = new Date(startDate);
                     endDate = new Date(endDate);
 
