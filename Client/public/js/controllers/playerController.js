280fc3699a233199709bb8c1aa5ea8aeab4a8cf5
victorPee <victorpee@snsoft.my>
2018-06-28 11:10:19 +0800

    Merge remote-tracking branch 'upstream/develop-1.1' into develop-1.1

--- Client/public/js/controllers/playerController.js
+++ Client/public/js/controllers/playerController.js
@@ -7572,7 +7572,7 @@ define(['js/app'], function (myApp) {
             let sendQuery = {
                 playerId: vm.selectedSinglePlayer._id,
                 platformId: vm.selectedSinglePlayer.platform,
-                lastLoginIp: vm.selectedSinglePlayer.lastLoginIp,
+                lastLoginIp: vm.selectedSinglePlayer.lastLoginIp || "",
                 index: newSearch ? 0 : vm.similarIpForPlayer.index,
                 limit: newSearch ? vm.similarIpForPlayer.limit : (vm.similarIpForPlayer.limit || 50),
                 sortCol: {registrationTime: -1},
