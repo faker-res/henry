6a06619388581b23aefed54bf21e16db2bf04ccc
yewhan <yewhan@snsoft.my>
2018-06-29 14:44:23 +0800

    #66-19 hide unused reward type

--- Server/db_modules/dbRewardType.js
+++ Server/db_modules/dbRewardType.js
@@ -2,6 +2,7 @@
 var dbconfig = require('./../modules/dbproperties');
 var encrypt = require('./../modules/encrypt');
 var dbRewardCondition = require('./dbRewardCondition');
+let constRewardType = require('./../const/constRewardType');
 
 var Q = require("q");
 
@@ -11,7 +12,29 @@ var dbRewardType = {
      * Get all reward types
      */
     getAllRewardType: function () {
-        return dbconfig.collection_rewardType.find({})
+        //hide unused reward type
+        let hideRewardType = [
+            constRewardType.FULL_ATTENDANCE,
+            constRewardType.PARTNER_CONSUMPTION_RETURN,
+            constRewardType.FIRST_TOP_UP,
+            constRewardType.PARTNER_INCENTIVE_REWARD,
+            constRewardType.PARTNER_REFERRAL_REWARD,
+            constRewardType.GAME_PROVIDER_REWARD,
+            constRewardType.PLATFORM_TRANSACTION_REWARD,
+            constRewardType.PLAYER_TOP_UP_RETURN,
+            constRewardType.PLAYER_CONSUMPTION_INCENTIVE,
+            constRewardType.PARTNER_TOP_UP_RETURN,
+            constRewardType.PLAYER_TOP_UP_REWARD,
+            constRewardType.PLAYER_REFERRAL_REWARD,
+            constRewardType.PLAYER_REGISTRATION_REWARD,
+            constRewardType.PLAYER_DOUBLE_TOP_UP_REWARD,
+            constRewardType.PLAYER_CONSECUTIVE_LOGIN_REWARD,
+            constRewardType.PLAYER_EASTER_EGG_REWARD,
+            constRewardType.PLAYER_TOP_UP_PROMO,
+            constRewardType.PLAYER_CONSECUTIVE_CONSUMPTION_REWARD,
+            constRewardType.PLAYER_PACKET_RAIN_REWARD,
+        ];
+        return dbconfig.collection_rewardType.find({name: {$nin: hideRewardType}})
             .populate({path: "params", model: dbconfig.collection_rewardParam})
             .populate({path: "condition", model: dbconfig.collection_rewardCondition}).exec();
     },
