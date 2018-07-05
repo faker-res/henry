3c167cddccc90f779df77a05dc3f6195f0df08a7
zheming <zheming@snsoft.my>
2018-07-05 16:23:29 +0800

    getOnlineTopupType bug fix

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -11164,6 +11164,10 @@ let dbPlayerInfo = {
                                                             type.maxDepositAmount = paymentData.merchants[i].permerchantLimits;
                                                         }
 
+                                                        if (type.status == 2 || paymentData.merchants[i].permerchantminLimits < type.minDepositAmount) {
+                                                            type.minDepositAmount = paymentData.merchants[i].permerchantminLimits;
+                                                        }
+
                                                         type.status = status;
                                                     }
                                                 }
