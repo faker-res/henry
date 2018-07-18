4e161a1c79a0229d8e49d3a3ca5c4732a17066a2
jykhow <jykhow@snsoft.my>
2018-07-18 17:21:11 +0800

    基础数据 ⁃> 代理提成设置 remove safeApply

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -26031,16 +26031,16 @@ define(['js/app'], function (myApp) {
                 }
 
                 socketService.$socket($scope.AppSocket, 'getPartnerCommissionConfig', sendData, function (data) {
-                    vm.partnerCommission.srcConfig = data.data;
-                    vm.partnerCommission.showConfig = data.data ? $.extend({}, data.data) : {};
-                    if (!Object.keys(vm.partnerCommission.showConfig).length) {
-                        vm.partnerCommission.showConfig = {};
-                        vm.partnerCommission.showConfig.commissionSetting = [];
-                        vm.commissionSettingNewRow(vm.partnerCommission.showConfig.commissionSetting);
+                    $scope.$evalAsync(() => {
+                        vm.partnerCommission.srcConfig = data.data;
+                        vm.partnerCommission.showConfig = data.data ? $.extend({}, data.data) : {};
+                        if (!Object.keys(vm.partnerCommission.showConfig).length) {
+                            vm.partnerCommission.showConfig = {};
+                            vm.partnerCommission.showConfig.commissionSetting = [];
+                            vm.commissionSettingNewRow(vm.partnerCommission.showConfig.commissionSetting);
 
-                    }
-
-                    $scope.safeApply();
+                        }
+                    });
                 });
             }
 
@@ -26580,44 +26580,45 @@ define(['js/app'], function (myApp) {
                 };
 
                 socketService.$socket($scope.AppSocket, 'getPartnerCommissionRateConfig', sendData, function (data) {
-                    if (data && data.data && data.data.length > 0) {
-                        data.data.forEach(config => {
-                            if (config.partner) {
-                                vm.custCommissionRateConfig.push(config);
-                            } else {
-                                // source config
-                                vm.srcCommissionRateConfig = config;
-                                vm.commissionRateConfig = JSON.parse(JSON.stringify(config));
+                    $scope.$evalAsync(() => {
+                        if (data && data.data && data.data.length > 0) {
+                            data.data.forEach(config => {
+                                if (config.partner) {
+                                    vm.custCommissionRateConfig.push(config);
+                                } else {
+                                    // source config
+                                    vm.srcCommissionRateConfig = config;
+                                    vm.commissionRateConfig = JSON.parse(JSON.stringify(config));
 
-                                vm.rateAfterRebatePromo = vm.commissionRateConfig.rateAfterRebatePromo;
-                                vm.rateAfterRebatePlatform = vm.commissionRateConfig.rateAfterRebatePlatform;
-                                if (vm.gameProviderGroup && vm.gameProviderGroup.length > 0) {
-                                    vm.gameProviderGroup.forEach(gameProviderGroup => {
-                                        let providerGroupRate = {gameProviderGroupId: gameProviderGroup._id, name: gameProviderGroup.name};
-                                        if (vm.commissionRateConfig && vm.commissionRateConfig.rateAfterRebateGameProviderGroup && vm.commissionRateConfig.rateAfterRebateGameProviderGroup.length > 0) {
-                                            vm.commissionRateConfig.rateAfterRebateGameProviderGroup.map(availableProviderGroupRate => {
-                                                if (gameProviderGroup._id == availableProviderGroupRate.gameProviderGroupId) {
-                                                    providerGroupRate = availableProviderGroupRate;
-                                                }
-                                            })
-                                        }
-                                        vm.rateAfterRebateGameProviderGroup.push(providerGroupRate);
-                                    })
+                                    vm.rateAfterRebatePromo = vm.commissionRateConfig.rateAfterRebatePromo;
+                                    vm.rateAfterRebatePlatform = vm.commissionRateConfig.rateAfterRebatePlatform;
+                                    if (vm.gameProviderGroup && vm.gameProviderGroup.length > 0) {
+                                        vm.gameProviderGroup.forEach(gameProviderGroup => {
+                                            let providerGroupRate = {gameProviderGroupId: gameProviderGroup._id, name: gameProviderGroup.name};
+                                            if (vm.commissionRateConfig && vm.commissionRateConfig.rateAfterRebateGameProviderGroup && vm.commissionRateConfig.rateAfterRebateGameProviderGroup.length > 0) {
+                                                vm.commissionRateConfig.rateAfterRebateGameProviderGroup.map(availableProviderGroupRate => {
+                                                    if (gameProviderGroup._id == availableProviderGroupRate.gameProviderGroupId) {
+                                                        providerGroupRate = availableProviderGroupRate;
+                                                    }
+                                                })
+                                            }
+                                            vm.rateAfterRebateGameProviderGroup.push(providerGroupRate);
+                                        })
+                                    }
+
+                                    vm.rateAfterRebateTotalDeposit = vm.commissionRateConfig.rateAfterRebateTotalDeposit;
+                                    vm.rateAfterRebateTotalWithdrawal = vm.commissionRateConfig.rateAfterRebateTotalWithdrawal;
+                                    vm.commissionRateConfig.isEditing = vm.commissionRateConfig.isEditing || {};
                                 }
-
-                                vm.rateAfterRebateTotalDeposit = vm.commissionRateConfig.rateAfterRebateTotalDeposit;
-                                vm.rateAfterRebateTotalWithdrawal = vm.commissionRateConfig.rateAfterRebateTotalWithdrawal;
-                                vm.commissionRateConfig.isEditing = vm.commissionRateConfig.isEditing || {};
-                            }
-                        })
-                    } else {
-                        if (vm.gameProviderGroup && vm.gameProviderGroup.length > 0) {
-                            vm.gameProviderGroup.forEach(gameProviderGroup => {
-                                vm.rateAfterRebateGameProviderGroup.push({gameProviderGroupId: gameProviderGroup._id, name: gameProviderGroup.name});
                             })
+                        } else {
+                            if (vm.gameProviderGroup && vm.gameProviderGroup.length > 0) {
+                                vm.gameProviderGroup.forEach(gameProviderGroup => {
+                                    vm.rateAfterRebateGameProviderGroup.push({gameProviderGroupId: gameProviderGroup._id, name: gameProviderGroup.name});
+                                })
+                            }
                         }
-                    }
-                    $scope.safeApply();
+                    });
                 });
             };
 
