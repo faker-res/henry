54a0cbbe311d0b9c51931f64371bb5b719281ad9
vincentsnsoft <vincent@snsoft.my>
2018-08-06 13:41:43 +0800

    add debug log

--- Server/db_modules/dbPlayerConsumptionWeekSummary.js
+++ Server/db_modules/dbPlayerConsumptionWeekSummary.js
@@ -749,6 +749,7 @@ var dbPlayerConsumptionWeekSummary = {
             ).then(
                 function (data) {
                     var res = {totalAmount: 0, totalConsumptionAmount: 0, event: eventObj};
+                    console.log("getPlayerConsumptionReturn:", res);
                     for (let amounts of data) {
                         Object.keys(amounts).forEach(
                             type => {
