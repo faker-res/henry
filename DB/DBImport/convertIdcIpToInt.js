8f807b1c85c6a7a5436fc2b29ddbbafc0bf24b45
ricco <ricco@snsoft.my>
2018-08-16 12:19:03 +0800

    idcip drop and convert function

---
+++ DB/DBImport/convertIdcIpToInt.js
@@ -0,0 +1,17 @@
+var db = db.getSiblingDB("admindb");
+
+var idcIps = db.idcIp.find().toArray();
+
+for (var i = 0; i < idcIps.length; i++) {
+    let thisObj = idcIps[i];
+    let thisObjId = thisObj._id;
+    thisObj.ip_start_num = ip2int(thisObj.ip_start);
+    thisObj.ip_end_num = ip2int(thisObj.ip_end);
+    delete thisObj._id;
+
+    db.idcIp.update({_id: thisObjId}, thisObj);
+}
+
+function ip2int(ip) {
+    return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
+}
