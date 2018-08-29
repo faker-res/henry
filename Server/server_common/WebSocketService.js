23f6a63189311830d36eb2143966b5facf876676
huatsnsoft <huat@snsoft.my>
2018-08-29 14:42:56 +0800

    [LOG] debug why the returned value did not go into appropriate listener

--- Server/server_common/WebSocketService.js
+++ Server/server_common/WebSocketService.js
@@ -558,7 +558,6 @@ var rootObj = {};
     proto.dispatchResponse = function (data) {
         if (!data)
             return;
-        console.log('WebSocketService dispatchResponse data', data);
         var listeners = this._requestListeners, listenersOnce = this._requestListenersOnce, i, len; //是否分前后顺序
         for (i = 0, len = listeners.length; i < len; i++) {
             if (listeners[i] && typeof listeners[i] === "function")
@@ -566,6 +565,7 @@ var rootObj = {};
         }
 
         var key = this.generateSyncKey(data);
+        console.log('WebSocketService dispatchResponse key and data', key, data, Object.keys(this._requestListenersOnceSync));
         if( key ){
             if( this._requestListenersOnceSync[key] ) {
                 for( var i = 0; i < this._requestListenersOnceSync[key].length; i++ ){
