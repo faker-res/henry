2cc5c5e0d2c35e6e99ba58a691c8a9934688ea87
mars <mars@snsoft.my>
2018-09-05 14:50:48 +0800

    Report export to excel - bug fixed.

--- Client/public/js/controllers/reportController.js
+++ Client/public/js/controllers/reportController.js
@@ -8236,19 +8236,6 @@ define(['js/app'], function (myApp) {
 
             // generate a download for xls
             vm.exportToExcel = function(tableId, reportName) {
-                var htmls = "";
-                var uri = 'data:application/vnd.ms-excel;base64,';
-                var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
-                var base64 = function(s) {
-                    return window.btoa(unescape(encodeURIComponent(s)))
-                };
-
-                var format = function(s, c) {
-                    return s.replace(/{(\w+)}/g, function(m, p) {
-                        return c[p];
-                    })
-                };
-
                 let tab = "";
                 let htmlContent = "";
 
@@ -8313,15 +8300,19 @@ define(['js/app'], function (myApp) {
                     htmlContent = "<tr>" + tab.getElementsByTagName('thead')[0].getElementsByTagName('tr')[0].innerHTML + "</tr>" + tab.getElementsByTagName('tbody')[0].innerHTML;
                 }
 
-                var ctx = {
-                    worksheet : 'Worksheet',
-                    table : htmlContent
-                }
+                var tab_text = '<html xmlns:x="urn:schemas-microsoft-com:office:excel">';
+                tab_text = tab_text + '<head><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>';
+                tab_text = tab_text + '<x:Name>Test Sheet</x:Name>';
+                tab_text = tab_text + '<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions></x:ExcelWorksheet>';
+                tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
+                tab_text = tab_text + "<table border='1px'>";
+                tab_text = tab_text + htmlContent;
+                tab_text = tab_text + '</table></body></html>';
+                var fileName = reportName ? $translate(reportName)+ ".xls": "FPMS report.xls";
 
-                var link = document.createElement("a");
-                link.download = reportName ? $translate(reportName)+ ".xls": "FPMS report.xls";
-                link.href = uri + base64(format(template, ctx));
-                link.click();
+                //Save the file
+                var blob = new Blob([tab_text], { type: "application/vnd.ms-excel;charset=utf-8" })
+                window.saveAs(blob, fileName);
             }
         }
 
