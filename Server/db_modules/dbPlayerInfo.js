1d7edcfc819ec35752cfb3ee403ab6e87ca750f3
vincentsnsoft <vincent@snsoft.my>
2018-09-05 14:56:17 +0800

    add debug log

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -4627,7 +4627,7 @@ let dbPlayerInfo = {
                 return {data: data[0], size: data[1]}
             },
             err => {
-                console.error("getPagePlayerByAdvanceQuery:", error);
+                console.error("getPagePlayerByAdvanceQuery:", err);
                 return {error: err};
             }
         );
