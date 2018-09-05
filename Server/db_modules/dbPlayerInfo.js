c2947778f6def37b715f9661b5a5cec295b35cd7
ricco <ricco@snsoft.my>
2018-09-05 17:10:40 +0800

    玩家个人支付宝收录历史

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -10627,6 +10627,7 @@ let dbPlayerInfo = {
                                                 "data.alipayer": callbackData ? callbackData.payer : "",
                                                 "data.alipayerAccount": callbackData ? callbackData.account : "",
                                                 "data.alipayerNickName": callbackData ? callbackData.nickName : "",
+                                                "data.alipayerRemark": callbackData ? callbackData.remark : "",
                                             }
                                         )
                                     );
