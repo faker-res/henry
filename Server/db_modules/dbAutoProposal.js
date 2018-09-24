6c27c5fc88502375c42bf8e9726fec600deec3e5
ricco <ricco@snsoft.my>
2018-09-24 15:40:41 +0800

    首提审核enhancement

--- Server/db_modules/dbAutoProposal.js
+++ Server/db_modules/dbAutoProposal.js
@@ -241,7 +241,10 @@ function checkRewardTaskGroup(proposal, platformObj) {
             if (data && data[6] && data[6].provinces && playerData && playerData.bankAccountProvince) {
                 let allProvinces = data[6].provinces;
                 let pIdx = allProvinces.findIndex(d => d.id === playerData.bankAccountProvince);
-                bankProvince = allProvinces[pIdx].name.substring(0, 2);
+
+                if (pIdx && allProvinces[pIdx] && allProvinces[pIdx].name) {
+                    bankProvince = allProvinces[pIdx].name.substring(0, 2);
+                }
             }
 
             let isApprove = true, canApprove = true;
