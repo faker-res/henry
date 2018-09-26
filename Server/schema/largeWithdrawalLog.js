6bff7c9bfb2d0e19052cf97de41ffe42c623d6b6
huatsnsoft <huat@snsoft.my>
2018-09-26 14:31:55 +0800

    add missing attribute for large withdrawal log

--- Server/schema/largeWithdrawalLog.js
+++ Server/schema/largeWithdrawalLog.js
@@ -32,6 +32,8 @@ let largeWithdrawalLogSchema = new Schema({
     comment: {type: String, default: ""},
     // player bonus amount / profit amount (current credit + current withdrawal amount - total top up amount between current withdrawal and last withdrawal)
     playerBonusAmount: {type: Number},
+    // player total top up amount
+    playerTotalTopUpAmount: {type: Number},
     // total consumption return amount after last withdrawal
     consumptionReturnAmount: {type: Number},
     // total reward amount that are not consumption return after last withdrawal
