2a53332605516e041aa5e0503a2c71793d07e631
mars <mars@snsoft.my>
2018-10-08 11:05:41 +0800

    #73-8.（手续费率在接口如何表示？）接口 createOnlineTopupProposal ，请新增字段『serviceCharge』 // 已经写文档

--- Server/db_modules/dbPlayerTopUpRecord.js
+++ Server/db_modules/dbPlayerTopUpRecord.js
@@ -732,6 +732,7 @@ var dbPlayerTopUpRecord = {
         let merchantGroupList = [];
         let rewardEvent;
         let newProposal;
+        let serviceChargeRate = 0;
 
         if (topupRequest.bonusCode && topUpReturnCode) {
             return Q.reject({
@@ -1050,6 +1051,7 @@ var dbPlayerTopUpRecord = {
                 return Promise.all([getRateProm]).then(
                     rate => {
                         if(rate && rate.length > 0 && typeof rate[0] != "undefined"){
+                            serviceCharge = rate[0];
                             updateData.data.rate = rate[0];
                             updateData.data.actualAmountReceived = (topupRequest.amount - (topupRequest.amount * Number(rate[0]))).toFixed(2);
                         }
@@ -1078,7 +1080,8 @@ var dbPlayerTopUpRecord = {
                     amount: topupRequest.amount,
                     createTime: proposalData.createTime,
                     status: proposalData.status,
-                    topupDetail: merchantResponse.result
+                    topupDetail: merchantResponse.result,
+                    serviceCharge: serviceCharge
                     //requestId: merchantResponse.result.requestId,
                     //result: merchantResponse.result,
                 };
