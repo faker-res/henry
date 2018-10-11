87073cc93516ad5c1434c566960450379f03b10d
marktai <mark@snsoft.my>
2018-10-11 14:38:19 +0800

    #74-7.自动回访 -> 回访任务总览 -> ( 下方）回访任务每日详情 ，以下两个栏位可以点击后延展详情。

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -611,7 +611,7 @@ define(['js/app'], function (myApp) {
                         }
                     }, 100);
                 });
-                
+
                 utilService.actionAfterLoaded("#partnerThemeSelectPanel #partnerThemeTable", function () {
                     setTimeout(()=>{
                         if (vm.selectedPlatform.data && vm.selectedPlatform.data.partnerThemeSetting) {
@@ -34164,6 +34164,8 @@ define(['js/app'], function (myApp) {
                             vm.autoFeedbackSearchDetailResult[scheduleNumber][date].acceptedCount = 0;
                             vm.autoFeedbackSearchDetailResult[scheduleNumber][date].loginCount = 0;
                             vm.autoFeedbackSearchDetailResult[scheduleNumber][date].topUpCount = 0;
+                            vm.autoFeedbackSearchDetailResult[scheduleNumber][date].loginPlayers = [];
+                            vm.autoFeedbackSearchDetailResult[scheduleNumber][date].topUpPlayers = [];
                             vm.autoFeedbackSearchDetailResult[scheduleNumber][date].data = [];
                         }
                     };
@@ -34186,9 +34188,12 @@ define(['js/app'], function (myApp) {
                                 }
                                 if(item.autoFeedbackMissionLogin) {
                                     vm.autoFeedbackSearchDetailResult[scheduleNumber][date].loginCount++;
+                                    vm.autoFeedbackSearchDetailResult[scheduleNumber][date].loginPlayers.push(item.playerName);
+
                                 }
                                 if(item.autoFeedbackMissionTopUp) {
                                     vm.autoFeedbackSearchDetailResult[scheduleNumber][date].topUpCount++;
+                                    vm.autoFeedbackSearchDetailResult[scheduleNumber][date].topUpPlayers.push(item.playerName);
                                 }
                                 vm.autoFeedbackSearchDetailResult[scheduleNumber][date].data.push(item);
                             }
@@ -34206,6 +34211,56 @@ define(['js/app'], function (myApp) {
                     $('#autoFeedbackDetailSpin').hide();
                 });
             };
+            vm.autoFeedbackShowLoginPlayers = function(data){
+
+                    var playerData = data.map((item, index)=>{ return { "no":index + 1,"playerName":item} })
+                    let tableOptions = {
+                        data: playerData,
+                        aoColumnDefs: [
+                            {targets: '_all', defaultContent: ' ', bSortable: false}
+                        ],
+                        columns: [
+                            {
+                                title: $translate('order'),
+                                data: "no"
+                            },
+                            {
+                                title: $translate('Account'),
+                                data: "playerName"
+                            }],
+                            "paging": true,
+                        };
+                        tableOptions = $.extend(true, {}, vm.generalDataTableOptions, tableOptions);
+                        utilService.createDatatableWithFooter('#autoFeedbackLoginPlayersTable', tableOptions, {}, true);
+                        utilService.actionAfterLoaded("#autoFeedbackLoginPlayersTable", function () {
+                            $('#autoFeedbackLoginPlayersTable').resize();
+                        })
+
+            };
+            vm.autoFeedbackShowTopUpPlayers = function(data){
+                    var playerData = data.map((item, index)=>{ return { "no":index + 1,"playerName":item} })
+                    let tableOptions = {
+                        data: playerData,
+                        aoColumnDefs: [
+                            {targets: '_all', defaultContent: ' ', bSortable: false}
+                        ],
+                        columns: [
+                            {
+                                title: $translate('order'),
+                                data: "no",
+                            },
+                            {
+                              title: $translate('Account'),
+                                data: "playerName"
+                            }],
+                            "paging": true,
+                        };
+                        tableOptions = $.extend(true, {}, vm.generalDataTableOptions, tableOptions);
+                        utilService.createDatatableWithFooter('#autoFeedbackTopUpPlayersTable', tableOptions, {}, true);
+                        utilService.actionAfterLoaded("#autoFeedbackTopUpPlayersTable", function () {
+                          $('#autoFeedbackTopUpPlayersTable').resize();
+                        })
+            }
             vm.autoFeedbackShowPromoCodeDetail = function(data) {
                 console.log(data);
                 vm.autoFeedbackPromoCodeDetail = data;
