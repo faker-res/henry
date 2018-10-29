59add65be5e5c5014b9d603774cd8e5f0001eede
zheming <zheming@snsoft.my>
2018-10-29 18:43:09 +0800

    script to sync player permission and promo code group

---
+++ Server/dataPatch/updateForbidPermission.js
@@ -0,0 +1,46 @@
+const dbconfig = require("../modules/dbproperties");
+
+const platformCursor = dbconfig.collection_platform.find({}).cursor();
+var i = 0;
+platformCursor.eachAsync(
+    platform => {
+        var playerQuery = {
+            platform: platform._id,
+            "permission.banReward": true
+        }
+        dbconfig.collection_players.distinct('name', playerQuery).lean().then(
+            playerName => {
+                if (playerName && playerName.length) {
+                    return dbconfig.collection_promoCodeUserGroup.findOneAndUpdate({
+                        platformObjId: platform._id,
+                        name: "Main Permission Disabled (default)", //hard code name
+                        isBlockByMainPermission: true,
+                        color: "lightgrey"
+                    }, {
+                        $addToSet: {playerNames: {$each: playerName}}
+                    },{upsert: true}).lean();
+                } else {
+                    return Promise.resolve();
+                }
+            }
+        ).then(
+            () => {
+                console.log('promo code group setting index', i);
+                i++;
+                return dbconfig.collection_promoCodeUserGroup.distinct("playerNames", {
+                    platformObjId: platform._id,
+                    isBlockPromoCodeUser: true
+                }).then(
+                    groupPlayers => {
+                        if (groupPlayers && groupPlayers.length) {
+                            dbconfig.collection_players.update({
+                                platform: platform._id,
+                                name: {$in: groupPlayers}
+                            }, {"forbidPromoCode": true}, {multi: true, new: true})
+                        }
+                    }
+                )
+            }
+        ).catch(err=> console.log("updateForbidPermission", err))
+    }
+);
