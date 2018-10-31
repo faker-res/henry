29480279f12202e28a4a0851e07902c24dae10e9
zheming <zheming@snsoft.my>
2018-10-31 12:11:40 +0800

    优惠分组 bug fix

--- Server/schema/rewardEventGroup.js
+++ Server/schema/rewardEventGroup.js
@@ -5,11 +5,13 @@ var rewardEventGroupSchema = new Schema({
     // platform of this group
     platform: {type: Schema.Types.ObjectId, ref: 'platform', index: true},
     // group name
-    name: {type: String, unique: true, required: true, index: true},
+    name: {type: String, required: true, index: true},
     // reward event attached to this group
     rewardEvents: [{type: Schema.ObjectId, ref: 'rewardEvent'}],
 
 });
+//record is unique by name and platform
+rewardEventGroupSchema.index({name: 1, platform: 1});
 
 module.exports = rewardEventGroupSchema;
 
