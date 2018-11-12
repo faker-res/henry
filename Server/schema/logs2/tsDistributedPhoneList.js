e704e8a6e52f178721f75b57e8ffa9c95c5ae3c7
huatsnsoft <huat@snsoft.my>
2018-11-12 15:41:56 +0800

    update tsdistributedphoneschema

--- Server/schema/logs2/tsDistributedPhoneList.js
+++ Server/schema/logs2/tsDistributedPhoneList.js
@@ -10,6 +10,20 @@ let tsDistributedPhoneListSchema = new Schema({
     tsPhoneList: {type: Schema.ObjectId, ref: 'tsPhoneList', index: true},
     // assignee (whose list is this)
     assignee: {type: Schema.Types.ObjectId, ref: 'admin', index: true},
+    // number of phone distributed
+    phoneCount: {type: Number, default: 0},
+    // number of phone used
+    phoneUsed: {type: Number, default: 0},
+    // number of Successful call
+    successfulCount: {type: Number, default: 0},
+    // number of registration
+    registrationCount: {type: Number, default: 0},
+    // number of single top up
+    singleTopUpCount: {type: Number, default: 0},
+    // number of player who top up multiple times that registered under this list
+    multipleTopUpCount: {type: Number, default: 0},
+    // effective player registered under this list
+    effectivePlayerCount: {type: Number, default: 0},
 
 });
 //record is unique by name and platform
