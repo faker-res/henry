547244da936405a22760805b3494cf2f3dd6ca8a
huatsnsoft <huat@snsoft.my>
2018-11-28 17:11:02 +0800

    电销群播任务 schema update

--- Server/schema/logs2/tsCallOutMissionCallee.js
+++ Server/schema/logs2/tsCallOutMissionCallee.js
@@ -2,21 +2,21 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 // demo player details
-let callOutMissionCalleeSchema = new Schema({
+let tsCallOutMissionCalleeSchema = new Schema({
     // platform
     platform:  {type: Schema.ObjectId, required: true, index: true},
     // adminObjId
     admin: {type: Schema.Types.ObjectId, ref: 'adminInfo', index: true},
     // missionObjId
-    mission: {type: Schema.Types.ObjectId, ref: 'callOutMission', index: true},
+    mission: {type: Schema.Types.ObjectId, ref: 'tsCallOutMission', index: true},
     // mission name
     missionName: {type: String, required: true, index: true},
     // index number (sorting of player on call)
     indexNo: {type: Number},
-    // playerObjId
-    player: {type: Schema.ObjectId, ref: 'player'},
-    // player name
-    playerName: {type: String, index: true},
+    // tsPhone
+    tsPhone: {type: Schema.ObjectId, ref: 'tsPhone'},
+    // tsDistributedPhone
+    tsDistributedPhone: {type: Schema.ObjectId, ref: 'tsDistributedPhone'},
     // phone number
     phoneNumber: {type: String},
     // calling time
@@ -31,4 +31,4 @@ let callOutMissionCalleeSchema = new Schema({
     data: {},
 });
 
-module.exports = callOutMissionCalleeSchema;
+module.exports = tsCallOutMissionCalleeSchema;
