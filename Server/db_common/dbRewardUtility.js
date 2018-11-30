65e649d03bd206fbf29a9e816f365db796bdeebd
ricco <ricco@snsoft.my>
2018-11-30 14:33:11 +0800

    Merge branch 'develop-1.1' of https://github.com/snsoft-my/FPMS into develop-1.1
    # Conflicts:
    #	Server/db_modules/dbPlayerReward.js

--- Server/db_common/dbRewardUtility.js
+++ Server/db_common/dbRewardUtility.js
@@ -479,7 +479,7 @@ const dbRewardUtility = {
         }
 
         let topupInPeriodProm = Promise.resolve([]);
-        if (rewardEvent.condition.topUpCountType && rewardEvent.condition) {
+        if (rewardEvent.condition && rewardEvent.condition.topUpCountType) {
             topupInPeriodProm = dbConfig.collection_playerTopUpRecord.find(topupMatchQuery).lean();
         }
 
@@ -491,7 +491,7 @@ const dbRewardUtility = {
 
                 let rewardPendingCount = checkList[0]
                 let eventInPeriodCount = checkList[1];
-                let topupInPeriodCount = isFrontEndApply ? checkList[2].length + 1 : checkList[2].length; /* if apply from font end, top up record + 1 to include current top up */
+                let topupInPeriodCount = isFrontEndApply ? checkList[2].length + 1 : checkList[2].length; /* if apply from font end together with top up, top up record + 1 to include current top up */
                 let listHasApplied = checkList[3];
 
                 // if there is a pending reward, then no other reward can be applied.
