d15f91699cccbb545297bca8794c8f69e08f30ab
marktai <mark@snsoft.my>
2018-12-10 16:57:49 +0800

    #77-13: 数字用toFixed(2)转换后变成string， 导致相减数字不对

--- Server/db_modules/dbGameProviderPlayerDaySummary.js
+++ Server/db_modules/dbGameProviderPlayerDaySummary.js
@@ -365,7 +365,7 @@ var dbGameProviderPlayerDaySummary = {
                 //1 - 数字相同不用补收录  2 - 需要补收录  3 - 重新收录中
                 let status = (cpmsData.validAmount - fpmsData.validAmount == 0) ? 1 : 2;
                 let validAmtSyncPercent = dbGameProviderPlayerDaySummary.getValidAmtSyncPercent(fpmsData.validAmount, cpmsData.validAmount);
-                let fpmsValidAmount = fpmsData.validAmount.toFixed(2);
+                let fpmsValidAmount = parseFloat(fpmsData.validAmount.toFixed(2));
                 result = {
                     providerId:proId,
                     fpmsConsumption:fpmsData.consumption,
