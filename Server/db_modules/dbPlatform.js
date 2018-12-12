3e5d3008504a87e5e5f4cf0a6ab04da2c3f7d6f1
yewhan <yewhan@snsoft.my>
2018-12-12 17:00:06 +0800

    短信三个接口测试 remove log

--- Server/db_modules/dbPlatform.js
+++ Server/db_modules/dbPlatform.js
@@ -3684,13 +3684,7 @@ var dbPlatform = {
     },
 
     getBlackWhiteListingConfig: (platformObjId) => {
-        console.log('platformObjId===', platformObjId);
-        return dbconfig.collection_platformBlackWhiteListing.findOne({platform: platformObjId}).lean().then(
-            result => {
-                console.log('result===', result);
-                return result;
-            }
-        );
+        return dbconfig.collection_platformBlackWhiteListing.findOne({platform: platformObjId}).lean();
     },
 
     saveBlackWhiteListingConfig: (platformObjId, updateData) => {
