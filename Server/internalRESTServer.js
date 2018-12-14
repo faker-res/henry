0387c16f67c0a6a92778725d5e849bf6326abe7d
ricco <ricco@snsoft.my>
2018-12-14 16:11:55 +0800

    debug

--- Server/internalRESTServer.js
+++ Server/internalRESTServer.js
@@ -3,12 +3,8 @@ const app = express();
 const port = 7100;
 const privateRoutes = require('./routes/privateAPI');
 const compression = require('compression');
-const bodyParser = require('body-parser');
-const http = require('http');
-const server = http.createServer(app);
 
 app.use(compression());
-// app.use(bodyParser.json());
 app.use('/', privateRoutes);
 
-server.listen(port, () => console.log(`Application listening on port ${port}!`));
+app.listen(port, () => console.log(`Application listening on port ${port}!`));
