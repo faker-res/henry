ee687f39ae4774f6d9ba70c0a7a53c7e84cc5ced
ricco <ricco@snsoft.my>
2018-12-18 15:38:46 +0800

    PMS通用充值接口

--- Server/db_modules/dbPlayerPayment.js
+++ Server/db_modules/dbPlayerPayment.js
@@ -483,7 +483,9 @@ const dbPlayerPayment = {
                 proposalData.playerLevel = player.playerLevel;
                 proposalData.platform = player.platform.platformId;
                 proposalData.playerName = player.name;
+                proposalData.playerRealName = player.realName;
                 proposalData.amount = Number(topupRequest.amount);
+                proposalData.bankCardGroupName = player.bankCardGroup && player.bankCardGroup.name || "";
                 proposalData.creator = entryType === "ADMIN" ? {
                     type: 'admin',
                     name: adminName,
