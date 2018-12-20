d62f5cb3f521efac33e9f1fa0e4052cb197b6bfc
huatsnsoft <huat@snsoft.my>
2018-12-20 17:05:25 +0800

    test fix XBET电子救援金计算问题 2018/12/20

--- Server/db_modules/dbPlayerReward.js
+++ Server/db_modules/dbPlayerReward.js
@@ -5675,6 +5675,7 @@ let dbPlayerReward = {
                 createTime: {$gte: todayTime.startTime, $lt: todayTime.endTime},
                 //bDirty: false,
                 playerId: playerData._id,
+                isDuplicate: false,
             };
 
             if (intervalTime) {
