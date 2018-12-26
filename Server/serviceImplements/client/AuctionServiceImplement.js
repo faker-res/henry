b871b60d3f2fd99e0b40778804072f0b77ad6949
ricco <ricco@snsoft.my>
2018-12-26 11:31:51 +0800

    temp comment

--- Server/serviceImplements/client/AuctionServiceImplement.js
+++ Server/serviceImplements/client/AuctionServiceImplement.js
@@ -1,39 +1,39 @@
-var WebSocketUtil = require("./../../server_common/WebSocketUtil");
-var AuctionService = require("./../../services/client/ClientServices").AuctionService;
-var dbAuction = require('./../../db_modules/dbAuction');
-var constSystemParam = require('../../const/constSystemParam');
-
-var AuctionServiceImplement = function(){
-    AuctionService.call(this);
-
-    this.listAuctionItems.expectsData = 'playerId: String';
-    this.listAuctionItems.onRequest = function(wsFunc, conn, data){
-        var isValidData = Boolean(conn.playerId);
-        WebSocketUtil.performAction(
-            conn, wsFunc, data, dbAuction.listAuctionItems, [data], isValidData
-        );
-    };
-
-    this.applyAuction.expectsData = 'playerId: String';
-    this.applyAuction.onRequest = function(wsFunc, conn, data){
-        var isValidData = Boolean(conn.playerId);
-        WebSocketUtil.performAction(
-            conn, wsFunc, data, dbAuction.applyAuction, [data], isValidData
-        );
-    };
-
-    this.isQualify.expectsData = 'playerId: String';
-    this.isQualify.onRequest = function(wsFunc, conn, data){
-        var isValidData = Boolean(conn.playerId);
-        WebSocketUtil.performAction(
-            conn, wsFunc, data, dbAuction.isQualify, [data], isValidData
-        );
-    };
-
-    // isQualify
-};
-
-var proto = AuctionServiceImplement.prototype = Object.create(AuctionService.prototype);
-proto.constructor = AuctionServiceImplement;
-
-module.exports = AuctionServiceImplement;
+// var WebSocketUtil = require("./../../server_common/WebSocketUtil");
+// var AuctionService = require("./../../services/client/ClientServices").AuctionService;
+// var dbAuction = require('./../../db_modules/dbAuction');
+// var constSystemParam = require('../../const/constSystemParam');
+//
+// var AuctionServiceImplement = function(){
+//     AuctionService.call(this);
+//
+//     this.listAuctionItems.expectsData = 'playerId: String';
+//     this.listAuctionItems.onRequest = function(wsFunc, conn, data){
+//         var isValidData = Boolean(conn.playerId);
+//         WebSocketUtil.performAction(
+//             conn, wsFunc, data, dbAuction.listAuctionItems, [data], isValidData
+//         );
+//     };
+//
+//     this.applyAuction.expectsData = 'playerId: String';
+//     this.applyAuction.onRequest = function(wsFunc, conn, data){
+//         var isValidData = Boolean(conn.playerId);
+//         WebSocketUtil.performAction(
+//             conn, wsFunc, data, dbAuction.applyAuction, [data], isValidData
+//         );
+//     };
+//
+//     this.isQualify.expectsData = 'playerId: String';
+//     this.isQualify.onRequest = function(wsFunc, conn, data){
+//         var isValidData = Boolean(conn.playerId);
+//         WebSocketUtil.performAction(
+//             conn, wsFunc, data, dbAuction.isQualify, [data], isValidData
+//         );
+//     };
+//
+//     // isQualify
+// };
+//
+// var proto = AuctionServiceImplement.prototype = Object.create(AuctionService.prototype);
+// proto.constructor = AuctionServiceImplement;
+//
+// module.exports = AuctionServiceImplement;
