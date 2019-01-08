603ea515b5c9a534c1b9718c5274c774428e12ea
jykhow <jykhow@snsoft.my>
2019-01-08 16:46:15 +0800

    代理结算点击取消直接刷新问题

--- Client/public/js/controllers/partnerController.js
+++ Client/public/js/controllers/partnerController.js
@@ -1174,7 +1174,22 @@ define(['js/app'], function (myApp) {
                     sendData.partnerCommLogId = partnerCommLogIdArr;
 
                     socketService.$socket($scope.AppSocket, 'cancelPartnerCommissionPreview', sendData, function (data) {
-                        getAllPartnerCommSettPreview();
+                        //getAllPartnerCommSettPreview();
+                        if (vm.allPartnerCommSettPreview && vm.allPartnerCommSettPreview.length > 0 && vm.selectedSettlePartnerCommPrev) {
+                            let tempPartnerCommSettPreview = [];
+                            vm.allPartnerCommSettPreview.forEach(preview => {
+                                if (preview && preview._id && vm.selectedSettlePartnerCommPrev && vm.selectedSettlePartnerCommPrev._id
+                                    && !(preview._id.toString() == vm.selectedSettlePartnerCommPrev._id.toString())) {
+                                    tempPartnerCommSettPreview.push(preview);
+                                }
+                            });
+
+                            if (tempPartnerCommSettPreview && tempPartnerCommSettPreview.length > 0) {
+                                vm.allPartnerCommSettPreview = tempPartnerCommSettPreview;
+                            }
+                        }
+                    }, function (error) {
+                        console.log('error cancelPartnerCommissionPreview',error)
                     });
                 }
             };
