c5b72d31fcf3dddb5ef3febfdb3dd7f57800b736
huatsnsoft <huat@snsoft.my>
2019-01-30 11:00:40 +0800

    自动过滤错误电话格式

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -19651,16 +19651,16 @@ let dbPlayerInfo = {
         let matchObj = {$match: {"phoneNumber": oldNewPhone, "platform": ObjectId(platformObjId)}};
 
 
-        // arrayPhoneXLS = arrayPhoneXLS.filter(phoneNumber => !isNaN(phoneNumber));
+        arrayPhoneXLS = arrayPhoneXLS.filter(phoneNumber => !isNaN(phoneNumber));
 
         for (let i = 0; i < arrayPhoneXLS.length; i++) {
             arrayPhoneXLS[i] = arrayPhoneXLS[i].trim();
-            if (isNaN(arrayPhoneXLS[i])) {
-                return Promise.reject({
-                    name: "DataError",
-                    message: "Invalid phone number"
-                });
-            }
+            // if (isNaN(arrayPhoneXLS[i])) {
+            //     return Promise.reject({
+            //         name: "DataError",
+            //         message: "Invalid phone number"
+            //     });
+            // }
             oldNewPhone.$in.push(arrayPhoneXLS[i]);
             oldNewPhone.$in.push(rsaCrypto.encrypt(arrayPhoneXLS[i]));
             oldNewPhone.$in.push(rsaCrypto.oldEncrypt(arrayPhoneXLS[i]));
