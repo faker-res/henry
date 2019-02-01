8af5b87da4d30cd7e5a5f7866bfd03c32ddc1825
victorpee <victorpee@snsoft.my>
2019-02-01 17:51:17 +0800

    added authServices with cookie creation function. (react-cookies plugin package)

---
+++ Mobile/src/services/authService.js
@@ -0,0 +1,61 @@
+import $ from 'jquery';
+import cookie from 'react-cookies';
+
+const auth = {
+    token: null,
+    adminId: null,
+    adminName: null,
+    department: null,
+    roleData: null,
+    allActions: null,
+    language: null
+}
+
+const authKey = {
+    cookieTokenKey: 'sinonet-management-token',
+    cookieAdminIdKey: 'sinonet-management-adminId',
+    cookieAdminNameKey: 'sinonet-management-adminName',
+    cookiePolicyKey: 'sinonet-management-role',
+    cookieDepartmentKey: 'sinonet-management-departments',
+    cookieLanguageKey: 'sinonet-management-language',
+    cookiePlatformKey: 'sinonet-management-platform'
+}
+
+let authService = {
+    
+    storeAuth: function(token, adminId, adminName, department, roleData, language, exp) {
+        cookie.save(authKey.cookieTokenKey, token, {
+            expires: exp,
+            path: '/'
+        });
+        cookie.save(authKey.cookieAdminIdKey, adminId, {
+            expires: exp,
+            path: '/'
+        });
+        cookie.save(authKey.cookieAdminNameKey, adminName, {
+            expires: exp,
+            path: '/'
+        });
+        cookie.save(authKey.cookieDepartmentKey, JSON.stringify(department), {
+            expires: exp,
+            path: '/'
+        });
+        cookie.save(authKey.cookieLanguageKey, language, {
+            expires: exp,
+            path: '/'
+        });
+        cookie.save(authKey.cookiePlatformKey, "XBet", {
+            expires: exp,
+            path: '/'
+        });
+        localStorage.setItem(authKey.cookiePolicyKey, JSON.stringify(roleData));
+        if (!auth.language) {
+            auth.language = "zh_CN";
+        }
+        
+        console.log(exp);
+        console.table(cookie.loadAll());
+    },
+}
+
+export default authService;
