ef28054162e9846a31256ad863674e930ddd3afb
talachmi <taylorlau@snsoft.my>
2019-02-01 15:43:15 +0800

    Changed all search filter component

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Login from './components/login';
+import Dashboard from './components/dashboard';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faIgloo, faAngleDown, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faSmile as fasFaSmile, faArrowAltCircleRight, faDollarSign, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import {faSmile as farFaSmile, faMoneyBillAlt, faRegistered, faStopCircle } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faIgloo, faAngleDown, faCheckSquare, faCoffee)
+library.add(faAngleDown, farFaSmile, fasFaSmile, faArrowAltCircleRight, faDollarSign, faMoneyBillAlt, faUserPlus, faRegistered, faStopCircle )
 
 class App extends Component {
+
     render() {
         return (
             <div>
                 <Login/>
+                <Dashboard/>
+
             </div>
         );
     }
