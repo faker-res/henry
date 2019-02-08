8b9cb45cdf22ac3385805f1f8baa4c026c0f7186
yewhan <yewhan@snsoft.my>
2019-02-08 09:26:19 +0800

    add null check

--- Server/db_modules/dbPlayerPayment.js
+++ Server/db_modules/dbPlayerPayment.js
@@ -782,8 +782,12 @@ function generatePMSHTTPUrl (playerData, proposalData, domain, clientType, ipAdd
         && extConfig && extConfig[playerData.platform.topUpSystemType] && extConfig[playerData.platform.topUpSystemType].topUpAPICallback) {
         paymentCallbackUrl = extConfig[playerData.platform.topUpSystemType].topUpAPICallback;
     }
-    console.log('playerData.platform.platformId===', playerData.platform.platformId);
-    console.log('playerData.platform.topUpSystemType===', playerData.platform.topUpSystemType);
+    if (playerData && playerData.platform && playerData.platform.platformId) {
+        console.log('playerData.platform.platformId===', playerData.platform.platformId);
+    }
+    if (playerData && playerData.platform && playerData.platform.topUpSystemType) {
+        console.log('playerData.platform.topUpSystemType===', playerData.platform.topUpSystemType);
+    }
     console.log('paymentCallbackUrl===', paymentCallbackUrl);
 
     url += "?";
