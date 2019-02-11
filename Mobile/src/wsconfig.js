986690225a69f3c713c1df9257374fa8d474f2c9
victorpee <victorpee@snsoft.my>
2019-02-11 15:56:12 +0800

    FPMS Mobile - update wsconfig file - delete package-lock.json

--- Mobile/src/wsconfig.js
+++ Mobile/src/wsconfig.js
@@ -1,12 +1,12 @@
 let WSCONFIG = {
     "pacnet": {
-        "socketURL": "papi-pacnet.fpms8.me:9000",
+        "socketURL": "papi-pacnet.fpms8.me:7000",
     },
     "globe": {
-        "socketURL": "papi-globe.fpms8.me:9000",
+        "socketURL": "papi-globe.fpms8.me:7000",
     },
     "wtt": {
-        "socketURL": "papi-wtt.fpms8.me:9000",
+        "socketURL": "papi-wtt.fpms8.me:7000",
     },
     "pccw": {
         "socketURL": "papi-pccw.fpms8.me:7000",
