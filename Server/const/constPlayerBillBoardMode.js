a44e96e1ef9ca6ebdcd889c6e7ff13be53861181
zheming <zheming@snsoft.my>
2019-02-27 11:52:10 +0800

    getPlayerBillBoard 新增模式（单笔输赢排行）

--- Server/const/constPlayerBillBoardMode.js
+++ Server/const/constPlayerBillBoardMode.js
@@ -4,7 +4,8 @@ const constPlayerBillBoardMode = {
     WITHDRAW_ALL: "3",
     VALIDBET_ALL: "4",
     WIN_ALL: "5",
-    WIN_SINGLE: "6"
+    WIN_SINGLE: "6", // multiple times
+    WIN_AMOUNT_SINGLE: "7"
 };
 
 module.exports = constPlayerBillBoardMode;
