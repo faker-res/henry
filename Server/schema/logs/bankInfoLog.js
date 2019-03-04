2497d578b07365a71a99c54fe8674df00369be5b
yewhan <yewhan@snsoft.my>
2019-03-04 11:33:57 +0800

    update bankInfoLog schema for multiple bank info

--- Server/schema/logs/bankInfoLog.js
+++ Server/schema/logs/bankInfoLog.js
@@ -23,6 +23,47 @@ var bankInfoLogSchema = new Schema({
     bankAddress: {type: String},
     //bank branch
     bankBranch: {type: String},
+
+    /*2nd bank info*/
+    //bank name， bankTypeId
+    bankName2: {type: String},
+    //bank account
+    bankAccount2: {type: String},
+    //bank account name
+    bankAccountName2: {type: String},
+    //bank account type
+    bankAccountType2: {type: String},
+    //bank account province
+    bankAccountProvince2: {type: String},
+    //bank account city
+    bankAccountCity2: {type: String},
+    //bank account district
+    bankAccountDistrict2: {type: String},
+    //full bank address
+    bankAddress2: {type: String},
+    //bank branch
+    bankBranch2: {type: String},
+
+    /*3rd bank info*/
+    //bank name， bankTypeId
+    bankName3: {type: String},
+    //bank account
+    bankAccount3: {type: String},
+    //bank account name
+    bankAccountName3: {type: String},
+    //bank account type
+    bankAccountType3: {type: String},
+    //bank account province
+    bankAccountProvince3: {type: String},
+    //bank account city
+    bankAccountCity3: {type: String},
+    //bank account district
+    bankAccountDistrict3: {type: String},
+    //full bank address
+    bankAddress3: {type: String},
+    //bank branch
+    bankBranch3: {type: String},
+
     targetObjectId: {type: Schema.ObjectId, required: true},
     //player or partner
     targetType: {type: String},
