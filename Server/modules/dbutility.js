2cfbd8542eba2b7c4ba82e61958db058e4707cef
ricco <ricco@snsoft.my>
2019-03-19 17:46:56 +0800

    db connection encrypt steps and encryption tool

--- Server/modules/dbutility.js
+++ Server/modules/dbutility.js
@@ -1112,6 +1112,13 @@ var dbUtility = {
         return filteredDomain;
     },
 
+    encryptMessage: (msg) => {
+        return new Promise((resolve, reject) => {
+            let encrypted = rsaCrypto.encrypt(msg);
+            resolve(encrypted);
+        })
+    },
+
     encodeEmail: function(email) {
         email = email || '';
         let emailChars = email.split('');
