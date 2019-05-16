2d0c0a65cf02bc4bcbbb78c91142f8807c12ce9e
Mark <mark@snsoft.my>
2019-05-16 14:54:18 +0800

    特别节日 - 少commit 了一个file

---
+++ Server/const/constFestivalRewardType.js
@@ -0,0 +1,11 @@
+/*
+ * Reward: PlayerFestivalRewardGroup's rewardType
+ */
+const constFestivalRewardType = {
+    FESTIVAL_TYPE_1: 1,
+    FESTIVAL_TYPE_2: 2,
+    FESTIVAL_TYPE_3: 3,
+    BIRTHDAY: 4
+};
+
+module.exports = constFestivalRewardType;
