e9319e5e4b4e7e8fec8e371fc5ae8a4f0d46c206
Mark <mark@snsoft.my>
2019-05-20 14:51:21 +0800

    网站业务-优惠代码-优惠类型-B监控；优惠类型-C监控，搜索条件优惠类型：加入复选

--- Server/db_modules/dbPlayerReward.js
+++ Server/db_modules/dbPlayerReward.js
@@ -4554,7 +4554,6 @@ let dbPlayerReward = {
             status: {$in: [constProposalStatus.APPROVED, constProposalStatus.SUCCESS]},
             "data.promoCodeTypeValue": isTypeCPromo? 3: {$ne: 3}
         };
-
         if (promoCodeTypeName) {
             promoCodeQuery["data.PROMO_CODE_TYPE"] = { $in : promoCodeTypeName };
         }
@@ -4573,7 +4572,6 @@ let dbPlayerReward = {
         ).then(
             promoCodeData => {
                 let delProm = [];
-
                 monitorObjs = promoCodeData.map(p => {
                     return {
                         promoCodeProposalId: p.proposalId,
@@ -4645,7 +4643,7 @@ let dbPlayerReward = {
             }
         ).then(
             () => {
-                let beginIndex = index * limit;
+                let beginIndex = index;
                 let playerArr = []; // to count total player
                 monitorObjs.forEach(item => {
                     if (item.playerName && !playerArr.includes(item.playerName)) {
