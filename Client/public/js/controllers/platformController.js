15f6631654af56a20334835f76186215257996c1
Ricco Tiong <ricco@snsoft.my>
2019-05-21 09:14:30 +0800

    Revert "特殊节日 - 对优惠尚未建立前， 添加节日做出报错提示"

--- Client/public/js/controllers/platformController.js
+++ Client/public/js/controllers/platformController.js
@@ -22603,10 +22603,6 @@ define(['js/app'], function (myApp) {
                 $scope.safeApply();
             };
             vm.addNewFestivalTitle = (festival) => {
-                if (!vm.showReward._id) {
-                    vm.addFestivalErrMsg = $translate("You need to this reward first before create festival item");
-                    return
-                }
                 let updateObj = {
                     'id': createObjectId(),
                     'name': festival.name,
@@ -26422,7 +26418,6 @@ define(['js/app'], function (myApp) {
                 $(tmpt).on('shown.bs.modal', function (e) {
                     $scope.$evalAsync();
                 })
-                vm.addFestivalErrMsg = '';
             }
 
             vm.showProposalModal = function (proposalId, templateNo) {
