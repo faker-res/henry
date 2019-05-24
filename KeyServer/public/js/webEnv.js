90c695a19d8cdf91b10fb50b4baf6ccec4b7abc5
ricco <ricco@snsoft.my>
2019-05-24 16:28:10 +0800

    keyServer changes

--- KeyServer/public/js/webEnv.js
+++ KeyServer/public/js/webEnv.js
@@ -1,9 +1,4 @@
 var webEnv = [
-    {nodeUrl: 'localhost', nodePort: '1802'},
-    {nodeUrl: 'localhost', nodePort: '1804'},
+    {nodeUrl: 'http://localhost', nodePort: '1802'},
+    {nodeUrl: 'http://localhost', nodePort: '1804'},
 ];
-
-// var webEnv = [
-//     {nodeUrl: 'cstest_fpms_sslserver.neweb.me', nodePort: '20014'},
-//     {nodeUrl: 'cstest_fpms_sslserver.neweb.me', nodePort: '20015'},
-// ];
