c1687450978638d50700b906acfa2ef7655fe254
yewhan <yewhan@snsoft.my>
2019-06-03 17:07:30 +0800

    promo code template - add column created by

--- Server/db_modules/dbPlayerReward.js
+++ Server/db_modules/dbPlayerReward.js
@@ -3339,7 +3339,7 @@ let dbPlayerReward = {
         ).catch(errorUtils.reportError);
     },
 
-    updatePromoCodeTemplate: (platformObjId, promoCodeTemplate) => {
+    updatePromoCodeTemplate: (platformObjId, promoCodeTemplate, adminInfo) => {
         let prom = [];
 
         promoCodeTemplate.forEach(entry => {
@@ -3357,6 +3357,12 @@ let dbPlayerReward = {
                     }))
                 }
                 else {
+                    if (!entry._id && !entry.adminId) { // for new entry
+                        entry.adminId = adminInfo.id;
+                    }
+                    if (!entry._id && !entry.adminName) { // for new entry
+                        entry.adminName = adminInfo.name;
+                    }
 
                     if (entry._id) {
                         delete entry._id;
