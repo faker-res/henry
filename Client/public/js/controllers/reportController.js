825f509ff8a8dc1211a235378df8b59e80a0e838
zhemingsnsoft <zheming@snsoft.my>
2019-06-03 20:17:18 +0800

    bug fix

--- Client/public/js/controllers/reportController.js
+++ Client/public/js/controllers/reportController.js
@@ -11126,6 +11126,7 @@ define(['js/app'], function (myApp) {
                     {group: "PARTNER", text: "createUpdatePartnerRealNameProposal", action: "createUpdatePartnerRealNameProposal"},
                     {group: "PARTNER", text: "generatePartnerCommSettPreview", action: "generatePartnerCommSettPreview"},
                     {group: "PARTNER", text: "cancelPartnerCommissionPreview", action: "cancelPartnerCommissionPreview"},
+                    {group: "PARTNER", text: "resetAllPartnerCustomizedCommissionRate", action: "resetAllPartnerCustomizedCommissionRate"},
 
                     {group: "Feedback", text: "ADD_FEEDBACK_RESULT", action: ["createPlayerFeedbackResult", "createPartnerFeedbackResult"]},
                     {group: "Feedback", text: "ADD_FEEDBACK_TOPIC", action: ["createPlayerFeedbackTopic", "createPartnerFeedbackTopic"]},
