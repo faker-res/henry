d536f2aceb780c2a772456520e1d0156d09f067b
YeeHuiSNSoft <yeehui@snsoft.my>
2019-06-04 13:50:34 +0800

    - 免费体验金组优惠异常 2019/06/0

--- Server/db_modules/dbPlayerReward.js
+++ Server/db_modules/dbPlayerReward.js
@@ -7403,7 +7403,7 @@ let dbPlayerReward = {
                     case constRewardType.PLAYER_FREE_TRIAL_REWARD_GROUP:
                         selectedRewardParam = selectedRewardParam[0];
 
-                        if (selectedRewardParam.rewardAmount && selectedRewardParam.spendingTimes) {
+                        if (selectedRewardParam.rewardAmount) {
                             // console.log('rewardSpecificData[0]',rewardSpecificData[0]); --- check 3 test results, need [1, 1, 1] to pass checking
                             let matchPlayerId = rewardSpecificData[0][0];
                             let matchIPAddress = rewardSpecificData[0][1];
@@ -7469,7 +7469,7 @@ let dbPlayerReward = {
                         }
 
                         rewardAmount = selectedRewardParam.rewardAmount;
-                        spendingAmount = selectedRewardParam.rewardAmount * selectedRewardParam.spendingTimes;
+                        spendingAmount = selectedRewardParam.rewardAmount * (selectedRewardParam.spendingTimes || 0);
                         break;
 
                     case constRewardType.PLAYER_FESTIVAL_REWARD_GROUP:
