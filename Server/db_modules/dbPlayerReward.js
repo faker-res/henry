d919cce344ecf31d7f17f75a2f96d824b63f54c3
Ricco Tiong <ricco@snsoft.my>
2019-06-13 10:59:31 +0800

    Revert "Revert " 输值返利组-定义输赢-（存-提-0点余额）中存款数据请改为使用【提交的存款金额，而不是扣除手续费后的金额】""

--- Server/db_modules/dbPlayerReward.js
+++ Server/db_modules/dbPlayerReward.js
@@ -6335,8 +6335,8 @@ let dbPlayerReward = {
                     );
 
                     let totalTopupMatchQuery = {
-                        playerId: playerData._id,
-                        platformId: playerData.platform._id,
+                        'data.playerName': playerData.name,
+                        'data.platformId': playerData.platform._id,
                         createTime: {$gte: todayTime.startTime, $lt: todayTime.endTime}
                     };
                     if (intervalTime) {
@@ -6350,14 +6350,14 @@ let dbPlayerReward = {
                     console.log("checking totalTopupMatchQuery", totalTopupMatchQuery)
 
 
-                    let totalTopupProm = dbConfig.collection_playerTopUpRecord.aggregate(
+                    let totalTopupProm = dbConfig.collection_proposal.aggregate(
                         {
                             $match: totalTopupMatchQuery
                         },
                         {
                             $group: {
-                                _id: {playerId: "$playerId"},
-                                amount: {$sum: "$amount"}
+                                _id: null,
+                                amount: {$sum: "$data.amount"}
                             }
                         }
                     ).then(
