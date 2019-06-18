6ca721d6ed886ce17c299e5620528bf234aad842
victorpee <victorpee@snsoft.my>
2019-06-18 14:19:27 +0800

    revert 优惠报表-其他优惠报表-游戏大厅不显示

--- Server/db_modules/dbProposal.js
+++ Server/db_modules/dbProposal.js
@@ -5314,7 +5314,6 @@ var proposal = {
         let totalPlayerCount = 0;
         let bonusResult = [];
         let depositResult = [];
-        let consumptionResult = [];
         let playerResult = [];
         let playerInfoResult = [];
         let resultSum = {
@@ -5394,35 +5393,7 @@ var proposal = {
 
                                 let depositProm = [];
                                 let bonusProm = [];
-                                let consumptionProm;
                                 let playerInfoProm;
-
-                                consumptionProm = dbconfig.collection_playerConsumptionRecord.aggregate([
-                                    {
-                                        $match: {
-                                            playerId: {$in: playerObjIdArr},
-                                            createTime: {
-                                                $gte: new Date(startTime),
-                                                $lt: new Date(endTime)
-                                            },
-                                            $or: [
-                                                {isDuplicate: {$exists: false}},
-                                                {
-                                                    $and: [
-                                                        {isDuplicate: {$exists: true}},
-                                                        {isDuplicate: false}
-                                                    ]
-                                                }
-                                            ]
-                                        }
-                                    },
-                                    {
-                                        $group: {
-                                            _id: "$playerId",
-                                            providerId: {$addToSet: "$providerId"}
-                                        }
-                                    }
-                                ]).allowDiskUse(true).read("secondaryPreferred");
 
                                 playerInfoProm = dbconfig.collection_players.find({_id: {$in: playerObjIdArr}},{registrationTime: 1, name: 1}).lean();
 
@@ -5497,7 +5468,7 @@ var proposal = {
                                                         })
 
                                                     }
-                                                    return Promise.all([Promise.all(bonusProm), Promise.all(depositProm), consumptionProm, playerInfoProm]);
+                                                    return Promise.all([Promise.all(bonusProm), Promise.all(depositProm), playerInfoProm]);
                                                 })
                                         }
                                     })
@@ -5550,14 +5521,13 @@ var proposal = {
                                         }
                                     ]).read("secondaryPreferred");
 
-                                    return Promise.all([bonusProm, depositProm, consumptionProm, playerInfoProm]);
+                                    return Promise.all([bonusProm, depositProm, playerInfoProm]);
                                 }
                             }
                         }).then( retResult => {
-                            if(retResult && retResult.length == 4){
+                            if(retResult && retResult.length == 3){
 
-                                consumptionResult = retResult[2];
-                                playerInfoResult = retResult[3];
+                                playerInfoResult = retResult[2];
 
                                 if (data.dayAfterReceiving){
 
@@ -5610,18 +5580,8 @@ var proposal = {
                                             player.totalDepositAmount = 0;
                                         }
 
-                                        if (consumptionResult && consumptionResult.length > 0){
-                                            let index = consumptionResult.findIndex( a => a._id.toString() == player._id.toString());
-                                            if (index != -1){
-                                                player.providerId = consumptionResult[index].providerId;
-                                                player.winLostAmount = player.totalDepositAmount - player.totalBonusAmount;
-                                                resultSum.winLostAmount += player.winLostAmount;
-                                            }
-                                            else{
-                                                player.winLostAmount = 0;
-                                                player.providerId = [];
-                                            }
-                                        }
+                                        player.winLostAmount = player.totalDepositAmount - player.totalBonusAmount;
+                                        resultSum.winLostAmount += player.winLostAmount;
 
                                         if (playerInfoResult && playerInfoResult.length > 0){
                                             let index = playerInfoResult.findIndex( a => a._id.toString() == player._id.toString());
