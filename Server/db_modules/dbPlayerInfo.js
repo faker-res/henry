1ff0353db3a8833ac4352201e83a2513d85d1a63
Mark <mark@snsoft.my>
2019-06-21 09:53:16 +0800

    add null check

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -633,7 +633,7 @@ let dbPlayerInfo = {
                     let playerIPRegionSplit = inputData.lastLoginIp.split('.');
                     let fromFontEnd = true;
                     if (adminName ||connPartnerId) {
-                        //if new player acc is created from backend or partner 
+                        //if new player acc is created from backend or partner
                         fromFontEnd = false;
                     }
                     ipData.forEach(ip => {
@@ -643,13 +643,13 @@ let dbPlayerInfo = {
                         }
                         let ipSplit = ip.ipAddress.split('.');
                         // compare 3 region of ip , for example :  10.1.1.182,  10.1.1.192<-> first 3 is equal ? yes, because 10.1.1 is same
-                        if (ipSplit[0] == playerIPRegionSplit[0] && ipSplit[0] != '') {
+                        if (ipSplit[0] && ipSplit[0] == playerIPRegionSplit[0] && ipSplit[0] != '') {
                             regionCount++;
                         }
-                        if (ipSplit[1] == playerIPRegionSplit[1] && ipSplit[1] != '') {
+                        if (ipSplit[1] && ipSplit[1] == playerIPRegionSplit[1] && ipSplit[1] != '') {
                             regionCount++;
                         }
-                        if (ipSplit[2] == playerIPRegionSplit[2] && ipSplit[2] != '') {
+                        if (ipSplit[2] && ipSplit[2] == playerIPRegionSplit[2] && ipSplit[2] != '') {
                             regionCount++;
                         }
 
