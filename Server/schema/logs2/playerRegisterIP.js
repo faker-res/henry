6bc843f201fb6ee5e3ac8034aea7717ef2334767
Mark <mark@snsoft.my>
2019-06-21 09:43:17 +0800

    add index for schema

--- Server/schema/logs2/playerRegisterIP.js
+++ Server/schema/logs2/playerRegisterIP.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var playerRegisterIP = new Schema({
-    platformObjId: {type: Schema.ObjectId, ref: 'platform', required: true},
+    platformObjId: {type: Schema.ObjectId, ref: 'platform', required: true, index: true},
 
     // record ipAddress when the player register
-    ipAddress: {type: String, required: true},
+    ipAddress: {type: String, required: true, index: true},
     // create Time
-    createTime: {type: Date, default: Date.now},
+    createTime: {type: Date, default: Date.now, index: true},
 });
 
 module.exports = playerRegisterIP;
