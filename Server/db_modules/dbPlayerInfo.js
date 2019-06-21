7cd33c6237ae1049f3058666ffe88848c29ec184
ricco <ricco@snsoft.my>
2019-06-21 17:53:19 +0800

    FPMS玩家报表问题 2019/06/18

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -18204,6 +18204,9 @@ let dbPlayerInfo = {
 
                 // Slice array to input page amount
                 if (returnedObj && returnedObj.data && returnedObj.data.length) {
+                    // Filter out players who has 0 topup and 0 bets
+                    returnedObj.data = returnedObj.data.filter(a => a.topUpTimes !== 0 || a.bonusTimes !== 0 || a.consumptionTimes !== 0);
+
                     returnedObj.data.sort((a, b) => sortBySortCol(a, b, sortCol));
                     returnedObj.data = returnedObj.data.slice(0, limit);
 
