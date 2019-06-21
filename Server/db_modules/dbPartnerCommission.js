3daf816b0ec791b1b8785833e0a2a8e80b9ada8e
huatsnsoft <huat@snsoft.my>
2019-06-21 11:13:16 +0800

    debug log

--- Server/db_modules/dbPartnerCommission.js
+++ Server/db_modules/dbPartnerCommission.js
@@ -272,16 +272,17 @@ const dbPartnerCommission = {
                     .round(2)
                     .done();
 
-                if (partner.partnerName == "plevel4") console.log("plevel4", 'consumptionAfterFeeMulti', consumptionAfterFeeMulti, totalConsumption, platformFeeMulti, rewardFeeMulti, topUpFeeMulti, withdrawalFeeMulti)
-                if (partner.partnerName == "plevel5") console.log("plevel5",'consumptionAfterFeeMulti', consumptionAfterFeeMulti, totalConsumption, platformFeeMulti, rewardFeeMulti, topUpFeeMulti, withdrawalFeeMulti)
+                // if (partner.partnerName == "plevel4") console.log("plevel4", 'consumptionAfterFeeMulti', consumptionAfterFeeMulti, totalConsumption, platformFeeMulti, rewardFeeMulti, topUpFeeMulti, withdrawalFeeMulti)
+                if (partner.partnerName == "plevel2") console.log("plevel2",'consumptionAfterFeeMulti', consumptionAfterFeeMulti, totalConsumption, platformFeeMulti, rewardFeeMulti, topUpFeeMulti, withdrawalFeeMulti)
             }
 
             let mainParentCommissionRate = multiLevelCommissionRate.parentRate[multiLevelCommissionRate.parentRate.length - 1] || multiLevelCommissionRate.commissionRate;
 
             let rawCommission = math.chain(consumptionAfterFeeMulti).multiply(multiLevelCommissionRate.commissionRate).round(2).done(); // this is useless for partner himself, only use to count relative partner's commission
-            if (partner.partnerName == "plevel4") console.log("plevel4", 'rawCommission', rawCommission, 'mainParentCommissionRate', mainParentCommissionRate)
-            if (partner.partnerName == "plevel5") console.log("plevel5",'rawCommission', rawCommission, 'mainParentCommissionRate', mainParentCommissionRate)
+            // if (partner.partnerName == "plevel4") console.log("plevel4", 'rawCommission', rawCommission, 'mainParentCommissionRate', mainParentCommissionRate)
+            // if (partner.partnerName == "plevel2") console.log("plevel2",'rawCommission', rawCommission, 'mainParentCommissionRate', mainParentCommissionRate)
             let rawDirectCommission = math.chain(consumptionAfterFeeDirect).multiply(directCommissionRate.commissionRate).round(2).done();
+            if (partner.partnerName == "plevel2") console.log("plevel2",'rawDirectCommission', rawDirectCommission, 'directCommissionRate.commissionRate', directCommissionRate.commissionRate)
 
             rawCommissions.push({
                 crewProfit: providerGroupConsumptionData[groupRate.groupName].bonusAmount,
@@ -316,8 +317,8 @@ const dbPartnerCommission = {
                 let parentRate = math.chain(multiLevelCommissionRate.parentRate[j] || 0).subtract(previousParentRate).round(8).done(); //multiLevelCommissionRate.parentRate[j] - previousParentRate;
                 previousParentRate = multiLevelCommissionRate.parentRate[j] || 0;
                 parentCommissionDetail[objId].rawCommissions = parentCommissionDetail[objId].rawCommissions || [];
-                if (partner.partnerName == "plevel4") console.log("plevel4",  'parentRatio', parentRatio, 'ratioSum', ratioSum)
-                if (partner.partnerName == "plevel5") console.log("plevel5", 'parentRatio', parentRatio, 'ratioSum', ratioSum)
+                // if (partner.partnerName == "plevel4") console.log("plevel4",  'parentRatio', parentRatio, 'ratioSum', ratioSum)
+                // if (partner.partnerName == "plevel2") console.log("plevel2", 'parentRatio', parentRatio, 'ratioSum', ratioSum)
 
                 let detail = {
                     groupName: groupRate.groupName,
@@ -336,8 +337,8 @@ const dbPartnerCommission = {
                     parentRatio = 1;
                 }
                 detail.amount = math.chain(rawCommission).multiply(parentRatio).round(2).done();
-                if (partner.partnerName == "plevel4") console.log("plevel4",  'amount', detail.amount)
-                if (partner.partnerName == "plevel5") console.log("plevel5", 'amount', detail.amount)
+                // if (partner.partnerName == "plevel4") console.log("plevel4",  'amount', detail.amount)
+                // if (partner.partnerName == "plevel2") console.log("plevel2", 'amount', detail.amount)
 
                 parentCommissionDetail[objId].grossCommission += detail.amount || 0;
                 parentCommissionDetail[objId].totalPlatformFee += detail.platformFee || 0;
