208427bb3e5cfe925904d5d36015224a0931f7ab
yewhan <yewhan@snsoft.my>
2019-06-28 16:41:14 +0800

    玩家-玩家列表-额度显示优化

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -5715,6 +5715,8 @@ let dbPlayerInfo = {
                                         playerData[ind].rewardPointsObjId = playerData[ind].rewardPointsObjId._id;
                                     }
 
+                                    playerData[ind].totalCredit = playerData[ind].validCredit + playerData[ind].lockedCredit;
+
                                     if (isProviderGroup) {
                                         newInfo = getRewardGroupData(playerData[ind]);
                                     } else {
