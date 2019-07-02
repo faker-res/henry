e3bf6ef8577c67ed91bc2a154fd6ebce726a4dd6
Ricco Tiong <ricco@snsoft.my>
2019-07-02 14:14:36 +0800

    Revert "Revert "14-16：报表-玩家存款分析报表、玩家存款追踪报表、电销开户报表 bug fix""

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -20265,6 +20265,11 @@ let dbPlayerInfo = {
                             });
                         },
                         processResponse: function (record) {
+                            if(record && record.data) {
+                                record.data.forEach(item => {
+                                    item.platform = platformObjId;
+                                })
+                            }
                             result = result.concat(record.data);
                         }
                     }
