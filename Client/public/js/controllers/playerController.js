ee6adc2f705d848eb8a95d4559084c0d77252400
yewhan <yewhan@snsoft.my>
2019-07-03 18:11:07 +0800

    remove decimal places, no rounding

--- Client/public/js/controllers/playerController.js
+++ Client/public/js/controllers/playerController.js
@@ -5107,7 +5107,7 @@ define(['js/app'], function (myApp) {
                             }
                         }
                         rowData.totalCredit = rowData.validCredit + rowData.lockedCredit;
-                        rowData.totalCredit.toFixed(2);
+                        rowData.totalCredit = Math.floor(rowData.totalCredit); // remove decimal places, no rounding
 
                         if (table) {
                             table.row.add(rowData);
