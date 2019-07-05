91f95f8eba36a39790b750fe9fff53caf4865677
yewhan <yewhan@snsoft.my>
2019-07-05 19:10:17 +0800

    玩家-玩家列表-额度显示优化 total credit needs to include credit transfer into lobby

--- Client/public/js/controllers/playerController.js
+++ Client/public/js/controllers/playerController.js
@@ -5108,7 +5108,7 @@ define(['js/app'], function (myApp) {
                                 rowData.platform$ = matchedPlatformData[0].name;
                             }
                         }
-                        rowData.totalCredit = rowData.validCredit + rowData.lockedCredit;
+                        rowData.totalCredit = rowData.validCredit + rowData.lockedCredit + rowData.totalTransferIn - rowData.totalTransferOut;
                         rowData.totalCredit = Math.floor(rowData.totalCredit); // remove decimal places, no rounding
 
                         if (table) {
