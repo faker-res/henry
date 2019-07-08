0526cb230611a61955f067aaba349e0217c81af4
ricco <ricco@snsoft.my>
2019-07-08 18:47:14 +0800

    提案报表尝试优化

--- Client/public/js/controllers/reportController.js
+++ Client/public/js/controllers/reportController.js
@@ -6630,7 +6630,7 @@ define(['js/app'], function (myApp) {
             }
 
             if (!newproposalQuery.platformList || !newproposalQuery.platformList.length) {
-                if (!vm.platformList || !vm.platformList.length) {
+                if (!vm.platformList || !vm.platformList.length || newproposalQuery.platformList.length === vm.platformList.length) {
                     return;
                 }
                 newproposalQuery.platformList = vm.platformList.map(platform => platform._id);
@@ -6651,7 +6651,7 @@ define(['js/app'], function (myApp) {
                 rewardTypeName: newproposalQuery.rewardTypeName,
                 promoTypeName: newproposalQuery.promoTypeName,
                 // platformId: vm.curPlatformId,
-                platformList: newproposalQuery.platformList && newproposalQuery.platformList.length ? newproposalQuery.platformList : [],
+                platformList: newproposalQuery.platformList ? newproposalQuery.platformList : [],
                 status: newproposalQuery.status,
                 relatedAccount: newproposalQuery.relatedAccount,
                 index: isExport ? 0 : (newSearch ? 0 : (newproposalQuery.index || 0)),
