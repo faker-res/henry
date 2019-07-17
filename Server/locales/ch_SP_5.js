6e305f0c6ebadb73f5e331dec8bd5711ea743657
Ricco Tiong <ricco@snsoft.my>
2019-07-17 10:12:50 +0800

    Revert "当等级未达到优惠条件时，增加报错提示 新增"

--- Server/locales/ch_SP_5.js
+++ Server/locales/ch_SP_5.js
@@ -18,7 +18,6 @@ var simplifiedChinese = {
     "Token is not authenticated": "令牌未经过身份验证",
     "Invalid token": "令牌无效",
     "Player ip doesn't match!": "玩家IP不匹配",
-    "Player does not reach level requirement for reward": "玩家未达到优惠申请等级",
     "The requirement is not fulfilled, please contact CS.": "不符合优惠要求，请联系客服人员",
     "Player must have a registered bank card to apply this reward": "此优惠需绑定真实提款银行资料后领取，谢谢",
     "Please register phone number and bank card before applying this reward, thank you.": "领取优惠请先绑定「电话」、「银行资料」， 谢谢。",
