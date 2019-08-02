49315e97e864671edc47da23798b251c3e9573b6
huatsnsoft <huat@snsoft.my>
2019-08-02 19:20:58 +0800

    add log callBackToUser

--- Server/db_modules/dbPlatform.js
+++ Server/db_modules/dbPlatform.js
@@ -4626,7 +4626,7 @@ var dbPlatform = {
                                 bodyJson = JSON.parse(String(bodyJson));
                             } catch (e) {
                                 console.error(e);
-                                console.error('bodyJson parse failure', bodyJson);
+                                console.error('bodyJson parse failure', link, bodyJson);
                             }
                             console.log('callBackToUser API json:', bodyJson, bodyJson.code, bodyJson.msg);
                             if (bodyJson && bodyJson.code == "0") {
