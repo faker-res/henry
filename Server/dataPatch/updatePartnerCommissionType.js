05cc9657e3f213597923b1ce07440cfbc37c8d0c
zhemingsnsoft <zheming@snsoft.my>
2019-08-05 13:58:37 +0800

    remove unused code

--- Server/dataPatch/updatePartnerCommissionType.js
+++ Server/dataPatch/updatePartnerCommissionType.js
@@ -14,17 +14,12 @@ dbconfig.collection_platform.findOne({platformId: platformId}, {_id: 1}).lean().
         if (!platform) {
             return Promise.reject("Cannot find platform")
         }
-        console.log("platformObjId ", platform._id)
         dbconfig.collection_partner.find({platform: platform._id, parent: null}).cursor().eachAsync(
             partner => {
                 if (partner && partner._id) {
-                    console.log('update partner commission index', i, partner.partnerName, partner.commissionType);
+                    console.log('update partner commission index', i, partner.partnerName);
                     i++;
-                    dbconfig.collection_partner.findOneAndUpdate({_id: partner._id, platform: partner.platform}, {$set: {commissionType: constPartnerCommissionType.WEEKLY_BONUS_AMOUNT}}).lean().then(
-                        data => {
-                            console.log("data", partner.partnerName, data.commissionType);
-                        }
-                    ).catch(err => {
+                    dbconfig.collection_partner.update({_id: partner._id, platform: partner.platform}, {commissionType: constPartnerCommissionType.WEEKLY_BONUS_AMOUNT}).catch(err => {
                         console.error(err)
                     });
                     dbPartnerCommissionConfig.updateMainPartnerCommissionData(partner.parent, partner._id, platform._id, constPartnerCommissionType.WEEKLY_BONUS_AMOUNT);
