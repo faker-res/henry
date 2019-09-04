b4fbc2a3f22b85863f4690840d711e56f8838795
Mark <mark@snsoft.my>
2019-09-04 16:15:40 +0800

    修正getApplicationReward - 特别节日领取资格判断

--- Server/db_modules/dbRewardEvent.js
+++ Server/db_modules/dbRewardEvent.js
@@ -3033,6 +3033,7 @@ var dbRewardEvent = {
                         console.log('MT --checking applyRewardSum', applyRewardSum);
 
                         festivalData.forEach(item => {
+                            item.date = ( ((Number(item.month) || 0) * 100 ) + Number(item.day || 0) );
                             if (item.minTopUpAmount &&  topUpSum < item.minTopUpAmount && item.status == true ) {
                                 item.status = false;
                             }
@@ -3041,6 +3042,7 @@ var dbRewardEvent = {
                                 item.status = false;
                             }
                         })
+                        festivalData.sort((a,b) => (a.date < b.date) ? -1 : ((b.date < a.date) ? 1 : 0));
                         festivalData.sort((a,b) => (a.status > b.status) ? -1 : ((b.status > a.status) ? 1 : 0));
                         returnData.result = festivalData;
 
@@ -3208,10 +3210,24 @@ var dbRewardEvent = {
         let proms = [];
         let festivalDate;
 
+        // hide those festival are missing most important field
+        selectedRewardParam = selectedRewardParam.map(festival => {
+            if ( festival.rewardType && (festival.rewardType == 3 || festival.rewardType == 6) && festival.totalConsumptionInInterval ) {
+                return festival;
+            } else if (festival.rewardType && (festival.rewardType != 3 && festival.rewardType != 6) && festival.applyTimes) {
+                return festival;
+            } else {
+                // console.log('festival no qualify :::', festival);
+                // if missing field, it wont be able to select by player
+            }
+
+        })
+        selectedRewardParam = selectedRewardParam.filter( item=>{ return item != undefined });
+
         if (selectedRewardParam && selectedRewardParam.length > 0) {
             selectedRewardParam.forEach( item => {
                 // apply a birthday festival
-                if ( item.rewardType == 4 || item.rewardType == 5 || item.rewardType == 6 ) {
+                if ( item.rewardType && (item.rewardType == 4 || item.rewardType == 5 || item.rewardType == 6) ) {
                     let birthday = getBirthday(playerBirthday);
                     console.log('MT --checking dbRewardEvent --birthday', birthday);
                     festivalDate = birthday;
@@ -3271,7 +3287,7 @@ var dbRewardEvent = {
                 if (data) {
                     console.log('rewardParam...', rewardParam)
                     let festival = dbRewardEvent.getFestivalName(rewardParam.festivalId, rewardParam.rewardType, eventData.param.others, DOB);
-                    // if the reward still available, and still on time to apply 
+                    // if the reward still available, and still on time to apply
                     if (rewardParam.applyTimes && data.length < rewardParam.applyTimes && isValidTime) {
                         console.log('***MT --checking can apply', 'now:', data.length, 'max:', rewardParam.applyTimes);
                         returnData = {status: true , festivalObjId: festivalId, name: festival.name, month:festival.month, day:festival.day, id: rewardParam.id, minTopUpAmount:rewardParam.minTopUpAmount || 0, spendingTimes:rewardParam.spendingTimes, rewardType:rewardParam.rewardType, expiredInDay: rewardParam.expiredInDay || 0 };
@@ -3279,6 +3295,9 @@ var dbRewardEvent = {
                             returnData.totalConsumptionInInterval = rewardParam.totalConsumptionInInterval;
                         }
                         resolve(returnData)
+                    } else if (rewardParam.applyTimes && data.length < rewardParam.applyTimes && !isValidTime) {
+                        console.log('***MT --checking cannot apply 2 and time not match', 'now:', data.length, 'max:', rewardParam.applyTimes);
+                        resolve({});
                     } else {
                         console.log('***MT --checking cannot apply', 'now:', data.length, 'max:', rewardParam.applyTimes);
                         returnData = {status: false, festivalObjId: festivalId, name: festival.name, month:festival.month, day:festival.day, id: rewardParam.id, minTopUpAmount:rewardParam.minTopUpAmount || 0, spendingTimes:rewardParam.spendingTimes, rewardType:rewardParam.rewardType, expiredInDay: rewardParam.expiredInDay || 0 };
@@ -3289,7 +3308,11 @@ var dbRewardEvent = {
                     }
                 } else {
                     console.log('***MT --checking festival proposal not found');
-                    resolve({status: false, festivalObjId: festivalId, name: festival.name, month:festival.month, day:festival.day, id: rewardParam.id, minTopUpAmount:rewardParam.minTopUpAmount || 0, spendingTimes:rewardParam.spendingTimes, rewardType:rewardParam.rewardType, expiredInDay: rewardParam.expiredInDay || 0 });
+                    if (isValidTime) {
+                        resolve({status: false, festivalObjId: festivalId, name: festival.name, month:festival.month, day:festival.day, id: rewardParam.id, minTopUpAmount:rewardParam.minTopUpAmount || 0, spendingTimes:rewardParam.spendingTimes, rewardType:rewardParam.rewardType, expiredInDay: rewardParam.expiredInDay || 0 });
+                    } else {
+                        resolve({});
+                    }
                 }
             })
         })
