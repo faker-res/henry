53172c395a07f2dbce862ccabcf8aab9fb550598
zhemingsnsoft <zheming@snsoft.my>
2019-10-11 12:08:19 +0800

    语音验证码更换第三方

--- Server/modules/dbutility.js
+++ Server/modules/dbutility.js
@@ -18,6 +18,7 @@ const queryPhoneLocationFromPackage = require('cellocate');
 const env = require('./../config/env').config();
 const rp = require('request-promise');
 const sha1 = require('sha1')
+const QcloudSms = require('qcloudsms_js');
 
 var dbUtility = {
 
@@ -87,64 +88,110 @@ var dbUtility = {
 
     //region Time
 
-    sendVoiceCode: function (phoneNumber, smsCode) {
-        let nonce = "";
-        let curTime = new Date().getTime();
-        const HEX_DIGITS = "0123456789abcdef";
+    sendVoiceCode: async function (phoneNumber, smsCode) {
+        // 语音消息应用 SDK AppID
+        var appid = env.voiceCodeSecret;  // SDK AppID 以1400开头
+        // 语音消息应用 App Key
+        var appkey = env.voiceCodeKEY;
+        // 需要发送语音消息的手机号码
+        // var phoneNumbers = phoneNumber;
+        // 语音模板 ID，需要在语音消息控制台中申请
+        // var templateId = 7839;  // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在语音消息控制台中申请
+        // 实例化 QcloudSms
+        var qcloudSms = QcloudSms(appid, appkey);
 
-        function checkSumBuilder(randomStr) {
-            let maxRand = randomStr.length - 1;
-            let minRand = 0;
-            for (let i = 0; i < 20; i++) {            //随机字符串最大128个字符，也可以小于该数
-                nonce += randomStr.charAt(Math.floor(Math.random() * (maxRand - minRand + 1)) + minRand);
-            }
-
-            let joinString = env.voiceCodeSecret + nonce + String(curTime);
-            return sha1(joinString);
-        }
-
-        let options = {
-            method: "POST",
-            uri: env.voiceCodeUrl,
-            headers: {
-                AppKey: env.voiceCodeKEY,
-                CurTime: String(curTime),
-                CheckSum: checkSumBuilder(HEX_DIGITS),
-                Nonce: nonce,
-                'Content-Type': "application/x-www-form-urlencoded"
-            },
-            form: {
-                mobile: phoneNumber,
-                authCode: Number(smsCode),
-                templateid: 14794553 // yun xin setting
-            },
-            json: true // Automatically stringifies the body to JSON
-        };
-
-        return rp(options).then(
-            data => {
-                if (!(data && data.code && data.code == 200)) {
-                    //315	IP限制
-                    //403	非法操作或没有权限
-                    //414	参数错误
-                    //416	频率控制
-                    //500	服务器内部错误
-                    console.log("send voice code error",data)
-                    return Promise.reject({
+        // return Promise.resolve({haha:"walao"})
+        let cvsender = qcloudSms.CodeVoiceSender();
+        // cvsender.send("86", phoneNumber, String(smsCode), 2, "", callback);
+        let prom = new Promise((resolve, reject) => {
+            cvsender.send("86", phoneNumber, String(smsCode), 2, "", (err, res, resData) => {
+                if (err) {
+                    console.log("send voice code failed", err)
+                    reject({
                         name: "DataError",
                         message: "Voice code failed to send, please contact customer service"
                     });
+                } else {
+                    if (resData && resData.result == 0) {
+                        resolve(resData);
+                    } else {
+                        console.log("send voice code failed", resData)
+                        reject({
+                            name: "DataError",
+                            message: "Voice code failed to send, please contact customer service"
+                        });
+                    }
+                    // console.log("request data: ", res.req);
+                    // console.log("response data: ", resData);
                 }
-                return data;
-            },
-            err => {
-                console.log("send voice code failed",err)
-                return Promise.reject({
-                    name: "DataError",
-                    message: "Voice code failed to send, please contact customer service"
-                });
-            }
-        )
+            });
+        })
+
+        return prom;
+
+        // 指定模板
+        // var templateId = 12345;
+        // var params = ["5678"];
+        // var tvsender = qcloudsms.TtsVoiceSender();
+        // tvsender.send("86", phoneNumbers[0], templateId, params, 2, "", callback);
+
+        // let nonce = "";
+        // let curTime = new Date().getTime();
+        // const HEX_DIGITS = "0123456789abcdef";
+        //
+        // function checkSumBuilder(randomStr) {
+        //     let maxRand = randomStr.length - 1;
+        //     let minRand = 0;
+        //     for (let i = 0; i < 20; i++) {            //随机字符串最大128个字符，也可以小于该数
+        //         nonce += randomStr.charAt(Math.floor(Math.random() * (maxRand - minRand + 1)) + minRand);
+        //     }
+        //
+        //     let joinString = env.voiceCodeSecret + nonce + String(curTime);
+        //     return sha1(joinString);
+        // }
+        //
+        // let options = {
+        //     method: "POST",
+        //     uri: env.voiceCodeUrl,
+        //     headers: {
+        //         AppKey: env.voiceCodeKEY,
+        //         CurTime: String(curTime),
+        //         CheckSum: checkSumBuilder(HEX_DIGITS),
+        //         Nonce: nonce,
+        //         'Content-Type': "application/x-www-form-urlencoded"
+        //     },
+        //     form: {
+        //         mobile: phoneNumber,
+        //         authCode: Number(smsCode),
+        //         templateid: 14794553 // yun xin setting
+        //     },
+        //     json: true // Automatically stringifies the body to JSON
+        // };
+        //
+        // return rp(options).then(
+        //     data => {
+        //         if (!(data && data.code && data.code == 200)) {
+        //             //315	IP限制
+        //             //403	非法操作或没有权限
+        //             //414	参数错误
+        //             //416	频率控制
+        //             //500	服务器内部错误
+        //             console.log("send voice code error",data)
+        //             return Promise.reject({
+        //                 name: "DataError",
+        //                 message: "Voice code failed to send, please contact customer service"
+        //             });
+        //         }
+        //         return data;
+        //     },
+        //     err => {
+        //         console.log("send voice code failed",err)
+        //         return Promise.reject({
+        //             name: "DataError",
+        //             message: "Voice code failed to send, please contact customer service"
+        //         });
+        //     }
+        // )
 
         // let options = {
         //     method: "POST",
