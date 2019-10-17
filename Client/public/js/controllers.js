174997879e8f2c88f7ed14b77cf40e5cac89dcec
YeeHuiSNSoft <yeehui@snsoft.my>
2019-10-17 11:16:17 +0800

    - 客服测试站【新开户查询】【尝试开户监控】查询数据有报错

--- Client/public/js/controllers.js
+++ Client/public/js/controllers.js
@@ -843,8 +843,8 @@ angular.module('myApp.controllers', ['ui.grid', 'ui.grid.edit', 'ui.grid.exporte
         4: 'H5_AGENT',
         5: 'APP_PLAYER',
         6: 'APP_AGENT',
-        // 7: 'APP_NATIVE_PLAYER',
-        // 8: 'APP_NATIVE_PARTNER',
+        7: 'APP_NATIVE_PLAYER',
+        8: 'APP_NATIVE_PARTNER',
     };
 
     $scope.constRewardPointsIntervalPeriod = {
