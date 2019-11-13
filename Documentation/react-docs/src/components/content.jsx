ac5b4e2172125f8a76c152e57fcdf989bdf280ac
talachmi <taylorlau@snsoft.my>
2019-11-13 10:04:46 +0800

    FPMS文檔template

--- Documentation/react-docs/src/components/content.jsx
+++ Documentation/react-docs/src/components/content.jsx
@@ -7,18 +7,36 @@ class Content extends Component{
 
     render(){
         return (
-            <div className="col-8">
+            <div className="col-7">
                 <div className="card">
                     <div className="card-header">
                         {this.props.linkBtn}
                     </div>
-                    <div className="card-body" style={{height: "250px"}}>
+                    <div className="card-body" >
+                        <h2>{this.props.title}</h2>
+                        <br></br>
+                        <h5>functionName: {this.props.functionName}</h5>
+                        <h5>{this.props.desc}</h5>
+                        <h5>请求内容:</h5>
+                        <table className="table table-bordered table-sm">
+                            {this.props.requestContent}
+                        </table>
+                        <h5>操作成功:</h5>
+                        <table className="table table-bordered table-sm">
+                            {this.props.statusOfSuccess}
+                        </table>
+                        <h5>操作失败:</h5>
+                        <table className="table table-bordered table-sm">
+                            {this.props.statusOfFailed}
+                        </table>
 
+
+                        {/*{this.props.data}*/}
                     </div>
                 </div>
-                <a href={this.props.url}>
-                    <h4>{this.props.name}</h4>
-                </a>
+                {/*<a href={this.props.url}>*/}
+                    {/*<h4>{this.props.name}</h4>*/}
+                {/*</a>*/}
             </div>
         )
     }
--- Documentation/react-docs/src/components/menu.jsx
+++ Documentation/react-docs/src/components/menu.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
+import apiData from '../data/apiDocumentation';
 
 class Menu extends Component{
     render(){
         return (
-            <div className="col-2">
+            <div className="col-4 col-sm-4 col-md-3 col-lg-2">
                 <div className="card">
                     <div className="card-body" style={{height: "100%"}}>
-                        <ul>
+                        <ul className="nav flex-column">
                             {this.props.list}
                         </ul>
                     </div>
