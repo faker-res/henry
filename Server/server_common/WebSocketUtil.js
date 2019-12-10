ce5809be813242d9544b8edc6ce8daa6f2459ee8
ruizhe <rzcheong@snsoft.my>
2019-12-10 14:38:19 +0800

    玩家禁止登入网站createGuestPlayer接口报错新增玩家信息

--- Server/server_common/WebSocketUtil.js
+++ Server/server_common/WebSocketUtil.js
@@ -182,8 +182,6 @@ var WebSocketUtility = {
                                 status: errorCode,
                                 errorMessage: $translate(err.message || err.errorMessage),
                                 data: serverInstance.getServerType() === "dataMigration" ? reqData : null,
-                                player: err.player,
-                                playerId: err.playerId
                             };
                             resObj.errorMessage = err.errMessage || resObj.errorMessage;
                             wsFunc.response(conn, resObj, reqData);
