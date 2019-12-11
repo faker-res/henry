0e14a6c05d8ae35a30d8aa05629d4fa4222f8df6
victorpee <victorpee@snsoft.my>
2019-12-11 11:48:00 +0800

    分析界面-数据加载速度优化 - 新玩家用户
    remove redundant query

--- Server/db_modules/dbPlayerInfo.js
+++ Server/db_modules/dbPlayerInfo.js
@@ -14089,10 +14089,7 @@ let dbPlayerInfo = {
             if (hasPartner == true) {
                 query.partner = {$type: "objectId"};
             } else {
-                query['$or'] = [
-                    {partner: null},
-                    {partner: {$exists: false}}
-                ]
+                query.partner = null;
             }
         }
         return dbconfig.collection_players.find(query).lean();
